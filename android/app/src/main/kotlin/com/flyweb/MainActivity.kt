598b78ed1b5420cad472dcf57b5ba3fbbf7b5712
Sirocco <admin@sirocco.fr>
2021-11-02 01:50:46 +0100

    com.mawaqit.admin -Alpha version for internal test

--- android/app/src/main/kotlin/com/flyweb/MainActivity.kt
+++ android/app/src/main/kotlin/com/flyweb/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.flyweb
+package com.mawaqit.admin
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ lib/generated_plugin_registrant.dart
@@ -0,0 +1,26 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: directives_ordering
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:connectivity_for_web/connectivity_for_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:uni_links_web/uni_links_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  ConnectivityPlugin.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UniLinksPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
