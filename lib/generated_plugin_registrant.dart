2cb5feb065d91a588ac14aafbb42ac836d540a83
ismaelAbogabal <63953869+ismaelAbogabal@users.noreply.github.com>
2022-04-26 19:17:13 +0200

    handle http error page
    show no internet screen when
    - no internet
    - backend server error
    
    show error message when setting load error on onboarding
    
    handle server down

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -6,8 +6,11 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:connectivity_for_web/connectivity_for_web.dart';
+import 'package:firebase_analytics_web/firebase_analytics_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:geolocator_web/geolocator_web.dart';
+import 'package:location_web/location_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:uni_links_web/uni_links_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -17,8 +20,11 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   ConnectivityPlugin.registerWith(registrar);
+  FirebaseAnalyticsWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   FirebaseMessagingWeb.registerWith(registrar);
+  GeolocatorPlugin.registerWith(registrar);
+  LocationWebPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   UniLinksPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
