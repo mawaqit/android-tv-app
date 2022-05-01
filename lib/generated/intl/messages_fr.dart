746f831b99dc28ae2ba04201e3cbfeefdf0ee807
ismaelAbogabal <63953869+ismaelAbogabal@users.noreply.github.com>
2022-05-01 19:59:49 +0200

    fix camel case issue
    - fix camel case issue
    - fix translation of dynamic pages (privacy policy, terms of use, installation guide)

---
+++ lib/generated/intl/messages_fr.dart
@@ -0,0 +1,104 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a fr locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'fr';
+
+  static String m0(mosqueId) =>
+      "${mosqueId} n\'est pas un id de mosquée valide";
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "about": MessageLookupByLibrary.simpleMessage("À propos"),
+        "appLang":
+            MessageLookupByLibrary.simpleMessage("Langue de l\'application"),
+        "backToHomePage": MessageLookupByLibrary.simpleMessage(
+            "Revenir à la page d\'accueil"),
+        "backendError": MessageLookupByLibrary.simpleMessage(
+            "Désolé, nous n\'avons pas pu nous connecter au serveur.\nVeuillez vérifier votre connectivité Internet ou réessayer plus tard."),
+        "cancel": MessageLookupByLibrary.simpleMessage("ANNULER"),
+        "changeMosque":
+            MessageLookupByLibrary.simpleMessage("Changer de Mosquée"),
+        "changeTheme": MessageLookupByLibrary.simpleMessage("Changer de thème"),
+        "closeApp":
+            MessageLookupByLibrary.simpleMessage("Fermer l\'application"),
+        "colorGradient":
+            MessageLookupByLibrary.simpleMessage("Dégradé de couleur"),
+        "colorSolid": MessageLookupByLibrary.simpleMessage("Couleur Solide"),
+        "customizeYourOwnWay": MessageLookupByLibrary.simpleMessage(
+            "Personnalisez votre propre option"),
+        "darkMode": MessageLookupByLibrary.simpleMessage("Mode sombre"),
+        "descLang": MessageLookupByLibrary.simpleMessage(
+            "Sélectionnez votre langue préférée."),
+        "drawerDesc": MessageLookupByLibrary.simpleMessage(
+            "Connecting muslims to Mosques"),
+        "drawerTitle": MessageLookupByLibrary.simpleMessage("MAWAQIT"),
+        "enterMosqueId": MessageLookupByLibrary.simpleMessage(
+            "Entrer l\'ID de la mosquée : "),
+        "facebook": MessageLookupByLibrary.simpleMessage("Facebook"),
+        "headerType": MessageLookupByLibrary.simpleMessage("Type d\'en-tête"),
+        "home": MessageLookupByLibrary.simpleMessage("Accueil"),
+        "instagram": MessageLookupByLibrary.simpleMessage("Instagram"),
+        "installationGuide":
+            MessageLookupByLibrary.simpleMessage("Guide d\'installation"),
+        "languages": MessageLookupByLibrary.simpleMessage("Langues"),
+        "leftButtonOption": MessageLookupByLibrary.simpleMessage(
+            "Options du bouton radio (à gauche)"),
+        "lightMode": MessageLookupByLibrary.simpleMessage("Mode clair"),
+        "loadingAnimation":
+            MessageLookupByLibrary.simpleMessage("Animation lors du lancement"),
+        "mawaqitDesc": MessageLookupByLibrary.simpleMessage(
+            "MAWAQIT vous offre une nouvelle façon de suivre et de gérer les temps de prière, en effet nous offrons un système de bout en bout qui fournit aux gestionnaires de mosquées un outil en ligne disponible 24/24h."),
+        "mawaqitWelcome":
+            MessageLookupByLibrary.simpleMessage("Bienvenue sur MAWAQIT"),
+        "messenger": MessageLookupByLibrary.simpleMessage("messager"),
+        "missingMosqueId": MessageLookupByLibrary.simpleMessage(
+            "Il manque l\'ID de la mosquée"),
+        "mosque": MessageLookupByLibrary.simpleMessage("Mosquée"),
+        "mosqueId": MessageLookupByLibrary.simpleMessage("ID de la mosquée "),
+        "mosqueIdIsNotValid": m0,
+        "navigationBarStyle": MessageLookupByLibrary.simpleMessage(
+            "Style des barres de navigation"),
+        "noInternet":
+            MessageLookupByLibrary.simpleMessage("Aucune connexion internet"),
+        "notification": MessageLookupByLibrary.simpleMessage("Notifications"),
+        "ok": MessageLookupByLibrary.simpleMessage("OK"),
+        "privacyPolicy": MessageLookupByLibrary.simpleMessage(
+            "Politique de confidentialité"),
+        "rate": MessageLookupByLibrary.simpleMessage("Laisser une évaluation"),
+        "rightButtonOption": MessageLookupByLibrary.simpleMessage(
+            "Options du bouton radio (à droite)"),
+        "selectMosqueId": MessageLookupByLibrary.simpleMessage(
+            "Sélectionner l\'ID de la mosquée"),
+        "share": MessageLookupByLibrary.simpleMessage("Partager"),
+        "skype": MessageLookupByLibrary.simpleMessage("Skype"),
+        "snapchat": MessageLookupByLibrary.simpleMessage("Snapchat"),
+        "sureCloseApp": MessageLookupByLibrary.simpleMessage(
+            "Voulez-vous vraiment quitter l\'application ?"),
+        "termsOfService": MessageLookupByLibrary.simpleMessage(
+            "Conditions générales d’utilisation"),
+        "tryAgain": MessageLookupByLibrary.simpleMessage("Réessayez"),
+        "twitter": MessageLookupByLibrary.simpleMessage("Twitter"),
+        "update": MessageLookupByLibrary.simpleMessage(
+            "Mettre à jour l\'application"),
+        "whatsApp": MessageLookupByLibrary.simpleMessage("WhatsApp"),
+        "whoops": MessageLookupByLibrary.simpleMessage("Oups !"),
+        "youtube": MessageLookupByLibrary.simpleMessage("Youtube")
+      };
+}
---
+++ lib/generated/intl/messages_it.dart
@@ -0,0 +1,101 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a it locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'it';
+
+  static String m0(mosqueId) => "${mosqueId} non è un ID moschea valido";
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "about": MessageLookupByLibrary.simpleMessage("Info su"),
+        "appLang":
+            MessageLookupByLibrary.simpleMessage("Lingua Dell\'Applicazione"),
+        "backToHomePage":
+            MessageLookupByLibrary.simpleMessage("Torna alla pagina iniziale"),
+        "backendError": MessageLookupByLibrary.simpleMessage(
+            "Spiacenti, non siamo riusciti a connetterci al server.\nVerifica la connettività Internet o riprova più tardi."),
+        "cancel": MessageLookupByLibrary.simpleMessage("Annullare"),
+        "changeMosque": MessageLookupByLibrary.simpleMessage("Cambia Moschea"),
+        "changeTheme": MessageLookupByLibrary.simpleMessage("Cambia tema"),
+        "closeApp": MessageLookupByLibrary.simpleMessage("Chiudi app"),
+        "colorGradient":
+            MessageLookupByLibrary.simpleMessage("Sfumatura Colore"),
+        "colorSolid": MessageLookupByLibrary.simpleMessage("Colore Solido"),
+        "customizeYourOwnWay": MessageLookupByLibrary.simpleMessage(
+            "Personalizzare la propria opzione"),
+        "darkMode": MessageLookupByLibrary.simpleMessage("Modalità scura"),
+        "descLang": MessageLookupByLibrary.simpleMessage(
+            "Seleziona la tua lingua preferita."),
+        "drawerDesc": MessageLookupByLibrary.simpleMessage(
+            "Connecting Muslims to Mosques"),
+        "drawerTitle": MessageLookupByLibrary.simpleMessage("MAWAQIT"),
+        "enterMosqueId": MessageLookupByLibrary.simpleMessage(
+            "Inserisci ID Della Moschea : "),
+        "facebook": MessageLookupByLibrary.simpleMessage("Facebook"),
+        "headerType":
+            MessageLookupByLibrary.simpleMessage("Tipo di intestazione"),
+        "home": MessageLookupByLibrary.simpleMessage("Pagina iniziale"),
+        "instagram": MessageLookupByLibrary.simpleMessage("Instagram"),
+        "installationGuide":
+            MessageLookupByLibrary.simpleMessage("Guida all\'installazione"),
+        "languages": MessageLookupByLibrary.simpleMessage("Lingue"),
+        "leftButtonOption": MessageLookupByLibrary.simpleMessage(
+            "Opzioni Pulsante Di Scelta (A Sinistra)"),
+        "lightMode": MessageLookupByLibrary.simpleMessage("Modalità luce"),
+        "loadingAnimation":
+            MessageLookupByLibrary.simpleMessage("Animazione di caricamento"),
+        "mawaqitDesc": MessageLookupByLibrary.simpleMessage(
+            "MAWAQIT ti offre un nuovo modo per tracciare e gestire i tempi di preghiera, infatti offriamo un sistema end-to-end che mette a disposizione dei gestori delle moschee uno strumento online disponibile 24/24h."),
+        "mawaqitWelcome":
+            MessageLookupByLibrary.simpleMessage("Benvenuto a MAWAQIT"),
+        "messenger": MessageLookupByLibrary.simpleMessage("Messenger"),
+        "missingMosqueId":
+            MessageLookupByLibrary.simpleMessage("Moschea ID mancante"),
+        "mosque": MessageLookupByLibrary.simpleMessage("Moschea"),
+        "mosqueId": MessageLookupByLibrary.simpleMessage("Moschea ID"),
+        "mosqueIdIsNotValid": m0,
+        "navigationBarStyle":
+            MessageLookupByLibrary.simpleMessage("Stile barra di navigazione"),
+        "noInternet": MessageLookupByLibrary.simpleMessage(
+            "Nessuna connessione internet"),
+        "notification": MessageLookupByLibrary.simpleMessage("Notifiche"),
+        "ok": MessageLookupByLibrary.simpleMessage("OK"),
+        "privacyPolicy":
+            MessageLookupByLibrary.simpleMessage("Politica sulla Riservatezza"),
+        "rate": MessageLookupByLibrary.simpleMessage("Votaci"),
+        "rightButtonOption": MessageLookupByLibrary.simpleMessage(
+            "Opzioni Pulsante Di Scelta (Destra)"),
+        "selectMosqueId":
+            MessageLookupByLibrary.simpleMessage("Seleziona Moschea ID"),
+        "share": MessageLookupByLibrary.simpleMessage("Condividi"),
+        "skype": MessageLookupByLibrary.simpleMessage("Skype"),
+        "snapchat": MessageLookupByLibrary.simpleMessage("Snapchat"),
+        "sureCloseApp": MessageLookupByLibrary.simpleMessage(
+            "Sei sicuro di che voler uscire dall\'applicazione?"),
+        "termsOfService":
+            MessageLookupByLibrary.simpleMessage("Condizioni del Servizio"),
+        "tryAgain": MessageLookupByLibrary.simpleMessage("Prova di nuovo"),
+        "twitter": MessageLookupByLibrary.simpleMessage("Twitter"),
+        "update": MessageLookupByLibrary.simpleMessage("Aggiorna Applicazione"),
+        "whatsApp": MessageLookupByLibrary.simpleMessage("WhatsApp"),
+        "whoops": MessageLookupByLibrary.simpleMessage("Cavolo!"),
+        "youtube": MessageLookupByLibrary.simpleMessage("YouTube")
+      };
+}
