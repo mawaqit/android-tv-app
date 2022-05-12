df7b833858eb5b61d6e8f5c2ec7722779683a7fa
Sirocco <admin@sirocco.fr>
2022-05-12 23:03:21 +0200

    lib/generated (after translation string update)

--- lib/generated/intl/messages_sq.dart
+++ lib/generated/intl/messages_sq.dart
@@ -7,8 +7,7 @@
 // ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
-// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
-// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+// ignore_for_file:unused_import, file_names
 
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
@@ -20,103 +19,73 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'sq';
 
-  static String m0(mosqueId) =>
-      "${mosqueId} nuk është një id i vlefshëm i xhamisë";
+  static m0(mosqueId) => "${mosqueId} nuk është një id i vlefshëm i xhamisë";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "about": MessageLookupByLibrary.simpleMessage("Rreth"),
-        "appLang": MessageLookupByLibrary.simpleMessage("Gjuha e Aplikacionit"),
-        "backToHomePage":
-            MessageLookupByLibrary.simpleMessage("Back to Homepage"),
-        "backendError": MessageLookupByLibrary.simpleMessage(
-            "Na vjen keq, nuk mundëm të lidheshim me serverin.\nJu lutemi verifikoni lidhjen me internetin ose provoni përsëri më vonë."),
-        "cancel": MessageLookupByLibrary.simpleMessage("ANULON"),
-        "changeMosque":
-            MessageLookupByLibrary.simpleMessage("Promijenite džamiju"),
-        "changeTheme": MessageLookupByLibrary.simpleMessage("Change Theme"),
-        "closeApp": MessageLookupByLibrary.simpleMessage("Mbyll aplikacionin"),
-        "colorGradient":
-            MessageLookupByLibrary.simpleMessage("Gradient Shumëngjyrësh"),
-        "colorSolid": MessageLookupByLibrary.simpleMessage("Color Solid"),
-        "customizeYourOwnWay":
-            MessageLookupByLibrary.simpleMessage("Customize your own option"),
-        "darkMode": MessageLookupByLibrary.simpleMessage("Modeliteti i errët"),
-        "descLang": MessageLookupByLibrary.simpleMessage(
-            "Përzgjidhni gjuhën tuaj të parapëlqyer."),
-        "doYouKnowMosqueId": MessageLookupByLibrary.simpleMessage(
-            "A e dini ID-në tuaj të instalimit apo ID-në tuaj të Xhamisë?"),
-        "drawerDesc": MessageLookupByLibrary.simpleMessage(
-            "Connecting Muslims to Mosques"),
-        "drawerTitle": MessageLookupByLibrary.simpleMessage("MAWAQIT"),
-        "enterMosqueId":
-            MessageLookupByLibrary.simpleMessage("Shkruani ID-në e Xhamisë : "),
-        "facebook": MessageLookupByLibrary.simpleMessage("Facebook"),
-        "gpsError": MessageLookupByLibrary.simpleMessage(
-            "Nuk mund të qaseni në vendndodhjen aktuale të pajisjes, ju lutemi kontrolloni nëse pajisja juaj GPS është e aktivizuar"),
-        "headerType": MessageLookupByLibrary.simpleMessage("Lloji i Headerit"),
-        "home": MessageLookupByLibrary.simpleMessage("Shtëpia"),
-        "instagram": MessageLookupByLibrary.simpleMessage("Instagram"),
-        "installationGuide":
-            MessageLookupByLibrary.simpleMessage("Udhëzuesi i instalimit"),
-        "languages": MessageLookupByLibrary.simpleMessage("Gjuha"),
-        "leftButtonOption":
-            MessageLookupByLibrary.simpleMessage("Radio Button Options (left)"),
-        "lightMode": MessageLookupByLibrary.simpleMessage("Mënyra e çelët"),
-        "loadingAnimation": MessageLookupByLibrary.simpleMessage(
-            "Duke ngarkuar animimin e qerthullit"),
-        "mawaqitDesc": MessageLookupByLibrary.simpleMessage(
-            "MAWAQIT ju ofron një mënyrë të re për të gjurmuar dhe menaxhuar kohët e namazit, në të vërtetë ne ofrojmë një sistem nga fundi në fund që u ofron menaxherëve të xhamive një mjet në internet të disponueshëm 24/24 orë."),
-        "mawaqitWelcome":
-            MessageLookupByLibrary.simpleMessage("Mirë se vini në MAWAQIT"),
-        "messenger": MessageLookupByLibrary.simpleMessage("Messenger"),
-        "missingMosqueId":
-            MessageLookupByLibrary.simpleMessage("Mungon ID e xhamisë"),
-        "mosque": MessageLookupByLibrary.simpleMessage("Džamija"),
-        "mosqueId": MessageLookupByLibrary.simpleMessage("ID e xhamisë "),
-        "mosqueIdIsNotValid": m0,
-        "mosqueInput": MessageLookupByLibrary.simpleMessage("Xhamia"),
-        "mosqueNameError":
-            MessageLookupByLibrary.simpleMessage("Shkruani emrin e xhamisë"),
-        "mosqueNoMore": MessageLookupByLibrary.simpleMessage(
-            "Na vjen keq, nuk ka më rezultate"),
-        "mosqueNoResults": MessageLookupByLibrary.simpleMessage(
-            "Na vjen keq, nuk ka rezultate"),
-        "navigationBarStyle":
-            MessageLookupByLibrary.simpleMessage("Navigation bars style"),
-        "no": MessageLookupByLibrary.simpleMessage("Jo"),
-        "noInternet": MessageLookupByLibrary.simpleMessage("Nuk ka internet"),
-        "notification": MessageLookupByLibrary.simpleMessage("Njoftim"),
-        "ok": MessageLookupByLibrary.simpleMessage("NE RREGULL"),
-        "privacyPolicy":
-            MessageLookupByLibrary.simpleMessage("Politika e privatësisë"),
-        "rate": MessageLookupByLibrary.simpleMessage("Na vlerso"),
-        "rightButtonOption": MessageLookupByLibrary.simpleMessage(
-            "Radio Button Options (right)"),
-        "searchForMosque":
-            MessageLookupByLibrary.simpleMessage("Kërkoni për një xhami"),
-        "searchMosque":
-            MessageLookupByLibrary.simpleMessage("Kërkoni për një xhami"),
-        "selectMosqueId":
-            MessageLookupByLibrary.simpleMessage("Shkruani ID-në e Xhamisë"),
-        "selectWithMosqueId":
-            MessageLookupByLibrary.simpleMessage("Shkruani ID-në e Xhamisë"),
-        "share": MessageLookupByLibrary.simpleMessage("Pjesë"),
-        "skype": MessageLookupByLibrary.simpleMessage("Skype"),
-        "slugError": MessageLookupByLibrary.simpleMessage(
-            "A nuk është një xhami e vlefshme \'slug\'"),
-        "snapchat": MessageLookupByLibrary.simpleMessage("Snapchat"),
-        "sureCloseApp": MessageLookupByLibrary.simpleMessage(
-            "Jeni i sigurt që dëshironi të dilni nga?"),
-        "termsOfService":
-            MessageLookupByLibrary.simpleMessage("Kushtet e Shërbimit"),
-        "tryAgain": MessageLookupByLibrary.simpleMessage("Pokušajte ponovno"),
-        "twitter": MessageLookupByLibrary.simpleMessage("Twitter"),
-        "update":
-            MessageLookupByLibrary.simpleMessage("Përditëso aplikacionin"),
-        "whatsApp": MessageLookupByLibrary.simpleMessage("WhatsApp"),
-        "whoops": MessageLookupByLibrary.simpleMessage("Oops!"),
-        "yes": MessageLookupByLibrary.simpleMessage("Po"),
-        "youtube": MessageLookupByLibrary.simpleMessage("Youtube")
-      };
+  static _notInlinedMessages(_) => <String, Function> {
+    "about" : MessageLookupByLibrary.simpleMessage("Rreth"),
+    "appLang" : MessageLookupByLibrary.simpleMessage("Gjuha e Aplikacionit"),
+    "backToHomePage" : MessageLookupByLibrary.simpleMessage("Back to Homepage"),
+    "backendError" : MessageLookupByLibrary.simpleMessage("Na vjen keq, nuk mundëm të lidheshim me serverin.\nJu lutemi verifikoni lidhjen me internetin ose provoni përsëri më vonë."),
+    "cancel" : MessageLookupByLibrary.simpleMessage("ANULON"),
+    "changeMosque" : MessageLookupByLibrary.simpleMessage("Promijenite džamiju"),
+    "changeTheme" : MessageLookupByLibrary.simpleMessage("Change Theme"),
+    "closeApp" : MessageLookupByLibrary.simpleMessage("Mbyll aplikacionin"),
+    "colorGradient" : MessageLookupByLibrary.simpleMessage("Gradient Shumëngjyrësh"),
+    "colorSolid" : MessageLookupByLibrary.simpleMessage("Color Solid"),
+    "customizeYourOwnWay" : MessageLookupByLibrary.simpleMessage("Customize your own option"),
+    "darkMode" : MessageLookupByLibrary.simpleMessage("Modeliteti i errët"),
+    "descLang" : MessageLookupByLibrary.simpleMessage("Përzgjidhni gjuhën tuaj të parapëlqyer."),
+    "doYouKnowMosqueId" : MessageLookupByLibrary.simpleMessage("A e dini ID-në tuaj të instalimit apo ID-në tuaj të Xhamisë?"),
+    "drawerDesc" : MessageLookupByLibrary.simpleMessage("Connecting Muslims to Mosques"),
+    "drawerTitle" : MessageLookupByLibrary.simpleMessage("MAWAQIT"),
+    "enterMosqueId" : MessageLookupByLibrary.simpleMessage("Shkruani ID-në e Xhamisë : "),
+    "facebook" : MessageLookupByLibrary.simpleMessage("Facebook"),
+    "gpsError" : MessageLookupByLibrary.simpleMessage("Nuk mund të qaseni në vendndodhjen aktuale të pajisjes, ju lutemi kontrolloni nëse pajisja juaj GPS është e aktivizuar"),
+    "headerType" : MessageLookupByLibrary.simpleMessage("Lloji i Headerit"),
+    "home" : MessageLookupByLibrary.simpleMessage("Shtëpia"),
+    "instagram" : MessageLookupByLibrary.simpleMessage("Instagram"),
+    "installationGuide" : MessageLookupByLibrary.simpleMessage("Udhëzuesi i instalimit"),
+    "languages" : MessageLookupByLibrary.simpleMessage("Gjuha"),
+    "leftButtonOption" : MessageLookupByLibrary.simpleMessage("Radio Button Options (left)"),
+    "lightMode" : MessageLookupByLibrary.simpleMessage("Mënyra e çelët"),
+    "loadingAnimation" : MessageLookupByLibrary.simpleMessage("Duke ngarkuar animimin e qerthullit"),
+    "mawaqitDesc" : MessageLookupByLibrary.simpleMessage("MAWAQIT ju ofron një mënyrë të re për të gjurmuar dhe menaxhuar kohët e namazit, në të vërtetë ne ofrojmë një sistem nga fundi në fund që u ofron menaxherëve të xhamive një mjet në internet të disponueshëm 24/24 orë."),
+    "mawaqitWelcome" : MessageLookupByLibrary.simpleMessage("Mirë se vini në MAWAQIT"),
+    "messenger" : MessageLookupByLibrary.simpleMessage("Messenger"),
+    "missingMosqueId" : MessageLookupByLibrary.simpleMessage("Mungon ID e xhamisë"),
+    "mosque" : MessageLookupByLibrary.simpleMessage("Džamija"),
+    "mosqueId" : MessageLookupByLibrary.simpleMessage("ID e xhamisë "),
+    "mosqueIdIsNotValid" : m0,
+    "mosqueInput" : MessageLookupByLibrary.simpleMessage("Xhamia"),
+    "mosqueNameError" : MessageLookupByLibrary.simpleMessage("Shkruani emrin e xhamisë"),
+    "mosqueNoMore" : MessageLookupByLibrary.simpleMessage("Na vjen keq, nuk ka më rezultate"),
+    "mosqueNoResults" : MessageLookupByLibrary.simpleMessage("Na vjen keq, nuk ka rezultate"),
+    "navigationBarStyle" : MessageLookupByLibrary.simpleMessage("Navigation bars style"),
+    "networkStatus" : MessageLookupByLibrary.simpleMessage("Statusi i rrjetit"),
+    "no" : MessageLookupByLibrary.simpleMessage("Jo"),
+    "noInternet" : MessageLookupByLibrary.simpleMessage("Nuk ka internet"),
+    "notification" : MessageLookupByLibrary.simpleMessage("Njoftim"),
+    "ok" : MessageLookupByLibrary.simpleMessage("NE RREGULL"),
+    "privacyPolicy" : MessageLookupByLibrary.simpleMessage("Politika e privatësisë"),
+    "rate" : MessageLookupByLibrary.simpleMessage("Na vlerso"),
+    "rightButtonOption" : MessageLookupByLibrary.simpleMessage("Radio Button Options (right)"),
+    "searchForMosque" : MessageLookupByLibrary.simpleMessage("Kërkoni për një xhami"),
+    "searchMosque" : MessageLookupByLibrary.simpleMessage("Kërkoni për një xhami"),
+    "selectMosqueId" : MessageLookupByLibrary.simpleMessage("Shkruani ID-në e Xhamisë"),
+    "selectWithMosqueId" : MessageLookupByLibrary.simpleMessage("Shkruani ID-në e Xhamisë"),
+    "share" : MessageLookupByLibrary.simpleMessage("Pjesë"),
+    "skype" : MessageLookupByLibrary.simpleMessage("Skype"),
+    "slugError" : MessageLookupByLibrary.simpleMessage("A nuk është një xhami e vlefshme \'slug\'"),
+    "snapchat" : MessageLookupByLibrary.simpleMessage("Snapchat"),
+    "sureCloseApp" : MessageLookupByLibrary.simpleMessage("Jeni i sigurt që dëshironi të dilni nga?"),
+    "termsOfService" : MessageLookupByLibrary.simpleMessage("Kushtet e Shërbimit"),
+    "tryAgain" : MessageLookupByLibrary.simpleMessage("Pokušajte ponovno"),
+    "twitter" : MessageLookupByLibrary.simpleMessage("Twitter"),
+    "update" : MessageLookupByLibrary.simpleMessage("Përditëso aplikacionin"),
+    "whatsApp" : MessageLookupByLibrary.simpleMessage("WhatsApp"),
+    "whoops" : MessageLookupByLibrary.simpleMessage("Oops!"),
+    "yes" : MessageLookupByLibrary.simpleMessage("Po"),
+    "youtube" : MessageLookupByLibrary.simpleMessage("Youtube")
+  };
 }
