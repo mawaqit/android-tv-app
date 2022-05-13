15303e4245754819a51e4a0ae9e4ff65a30deee8
Sirocco <admin@sirocco.fr>
2022-05-13 16:45:31 +0200

    lib/generated (after translation string update)

--- lib/generated/intl/messages_nl.dart
+++ lib/generated/intl/messages_nl.dart
@@ -7,7 +7,8 @@
 // ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
-// ignore_for_file:unused_import, file_names
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
 
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
@@ -19,73 +20,99 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'nl';
 
-  static m0(mosqueId) => "${mosqueId} is geen geldige Moske-ID";
+  static String m0(mosqueId) => "${mosqueId} is geen geldige Moske-ID";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static _notInlinedMessages(_) => <String, Function> {
-    "about" : MessageLookupByLibrary.simpleMessage("Informatie"),
-    "appLang" : MessageLookupByLibrary.simpleMessage("App taal"),
-    "backToHomePage" : MessageLookupByLibrary.simpleMessage("Terug naar Startpagina"),
-    "backendError" : MessageLookupByLibrary.simpleMessage("Sorry, we konden geen verbinding maken met de server.\nControleer de internetverbinding of probeer het later opnieuw."),
-    "cancel" : MessageLookupByLibrary.simpleMessage("Annuleren"),
-    "changeMosque" : MessageLookupByLibrary.simpleMessage("Wijzigen Moskee"),
-    "changeTheme" : MessageLookupByLibrary.simpleMessage("Thema wijzigen"),
-    "closeApp" : MessageLookupByLibrary.simpleMessage("App sluiten"),
-    "colorGradient" : MessageLookupByLibrary.simpleMessage("Kleur verloop"),
-    "colorSolid" : MessageLookupByLibrary.simpleMessage("Kleur Solide"),
-    "customizeYourOwnWay" : MessageLookupByLibrary.simpleMessage("Pas je eigen manier aan"),
-    "darkMode" : MessageLookupByLibrary.simpleMessage("Donkere modus"),
-    "descLang" : MessageLookupByLibrary.simpleMessage("Kies één of meer voorkeurstalen."),
-    "doYouKnowMosqueId" : MessageLookupByLibrary.simpleMessage("Ken je je installatie-ID of je Moske-ID?"),
-    "drawerDesc" : MessageLookupByLibrary.simpleMessage("Connecting Muslims to Mosques"),
-    "drawerTitle" : MessageLookupByLibrary.simpleMessage("MAWAQIT"),
-    "enterMosqueId" : MessageLookupByLibrary.simpleMessage("Voer Moske-ID in: "),
-    "facebook" : MessageLookupByLibrary.simpleMessage("Facebook"),
-    "gpsError" : MessageLookupByLibrary.simpleMessage("Geen toegang tot de huidige locatie van het apparaat, controleer of de GPS is ingeschakeld"),
-    "headerType" : MessageLookupByLibrary.simpleMessage("Header type"),
-    "home" : MessageLookupByLibrary.simpleMessage("Startpagina"),
-    "instagram" : MessageLookupByLibrary.simpleMessage("Instagram"),
-    "installationGuide" : MessageLookupByLibrary.simpleMessage("Installatiehandleiding"),
-    "languages" : MessageLookupByLibrary.simpleMessage("Leren"),
-    "leftButtonOption" : MessageLookupByLibrary.simpleMessage("Radio Knop opties (links)"),
-    "lightMode" : MessageLookupByLibrary.simpleMessage("Lichte modus"),
-    "loadingAnimation" : MessageLookupByLibrary.simpleMessage("Animatie laden"),
-    "mawaqitDesc" : MessageLookupByLibrary.simpleMessage("MAWAQIT biedt je een nieuwe manier om gebedstijden bij te houden en te beheren. We bieden inderdaad een end-to-end systeem dat moskeemanagers een online tool biedt die 24/24 uur beschikbaar is."),
-    "mawaqitWelcome" : MessageLookupByLibrary.simpleMessage("Welkom bij MAWAQIT"),
-    "messenger" : MessageLookupByLibrary.simpleMessage("berichtgever"),
-    "missingMosqueId" : MessageLookupByLibrary.simpleMessage("Ontbrekende moske-ID"),
-    "mosque" : MessageLookupByLibrary.simpleMessage("Moskee"),
-    "mosqueId" : MessageLookupByLibrary.simpleMessage("Moske-ID "),
-    "mosqueIdIsNotValid" : m0,
-    "mosqueInput" : MessageLookupByLibrary.simpleMessage("Moskee input"),
-    "mosqueNameError" : MessageLookupByLibrary.simpleMessage("Ga de Moske-naam in"),
-    "mosqueNoMore" : MessageLookupByLibrary.simpleMessage("Niet meer resultaten"),
-    "mosqueNoResults" : MessageLookupByLibrary.simpleMessage("Geen resultaten"),
-    "navigationBarStyle" : MessageLookupByLibrary.simpleMessage("Stijl navigatiebalken"),
-    "networkStatus" : MessageLookupByLibrary.simpleMessage("Netwerk Status"),
-    "no" : MessageLookupByLibrary.simpleMessage("Nee"),
-    "noInternet" : MessageLookupByLibrary.simpleMessage("Geen internetverbinding"),
-    "notification" : MessageLookupByLibrary.simpleMessage("Meldingen"),
-    "ok" : MessageLookupByLibrary.simpleMessage("OK"),
-    "privacyPolicy" : MessageLookupByLibrary.simpleMessage("Privacybeleid"),
-    "rate" : MessageLookupByLibrary.simpleMessage("Beoordeel ons"),
-    "rightButtonOption" : MessageLookupByLibrary.simpleMessage("Radio Knop opties (links)"),
-    "searchForMosque" : MessageLookupByLibrary.simpleMessage("Zoek een moskee"),
-    "searchMosque" : MessageLookupByLibrary.simpleMessage("Zoek een moskee"),
-    "selectMosqueId" : MessageLookupByLibrary.simpleMessage("Voer Moske-ID in"),
-    "selectWithMosqueId" : MessageLookupByLibrary.simpleMessage("Voer Moske-ID in"),
-    "share" : MessageLookupByLibrary.simpleMessage("Delen"),
-    "skype" : MessageLookupByLibrary.simpleMessage("Skype"),
-    "slugError" : MessageLookupByLibrary.simpleMessage("Dit is geen geldige moskee slug"),
-    "snapchat" : MessageLookupByLibrary.simpleMessage("Snapchat"),
-    "sureCloseApp" : MessageLookupByLibrary.simpleMessage("Weet u zeker dat u de toepassing wilt afsluiten?"),
-    "termsOfService" : MessageLookupByLibrary.simpleMessage("Servicevoorwaarden"),
-    "tryAgain" : MessageLookupByLibrary.simpleMessage("Probeer opnieuw"),
-    "twitter" : MessageLookupByLibrary.simpleMessage("Twitter"),
-    "update" : MessageLookupByLibrary.simpleMessage("Toepassing bijwerken"),
-    "whatsApp" : MessageLookupByLibrary.simpleMessage("WhatsApp"),
-    "whoops" : MessageLookupByLibrary.simpleMessage("Oeps!"),
-    "yes" : MessageLookupByLibrary.simpleMessage("Ja"),
-    "youtube" : MessageLookupByLibrary.simpleMessage("YouTube")
-  };
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "about": MessageLookupByLibrary.simpleMessage("Informatie"),
+        "appLang": MessageLookupByLibrary.simpleMessage("App taal"),
+        "backToHomePage":
+            MessageLookupByLibrary.simpleMessage("Terug naar Startpagina"),
+        "backendError": MessageLookupByLibrary.simpleMessage(
+            "Sorry, we konden geen verbinding maken met de server.\nControleer de internetverbinding of probeer het later opnieuw."),
+        "cancel": MessageLookupByLibrary.simpleMessage("Annuleren"),
+        "changeMosque": MessageLookupByLibrary.simpleMessage("Wijzigen Moskee"),
+        "changeTheme": MessageLookupByLibrary.simpleMessage("Thema wijzigen"),
+        "closeApp": MessageLookupByLibrary.simpleMessage("App sluiten"),
+        "colorGradient": MessageLookupByLibrary.simpleMessage("Kleur verloop"),
+        "colorSolid": MessageLookupByLibrary.simpleMessage("Kleur Solide"),
+        "customizeYourOwnWay":
+            MessageLookupByLibrary.simpleMessage("Pas je eigen manier aan"),
+        "darkMode": MessageLookupByLibrary.simpleMessage("Donkere modus"),
+        "descLang": MessageLookupByLibrary.simpleMessage(
+            "Kies één of meer voorkeurstalen."),
+        "doYouKnowMosqueId": MessageLookupByLibrary.simpleMessage(
+            "Ken je je installatie-ID of je Moske-ID?"),
+        "drawerDesc": MessageLookupByLibrary.simpleMessage(
+            "Connecting Muslims to Mosques"),
+        "drawerTitle": MessageLookupByLibrary.simpleMessage("MAWAQIT"),
+        "enterMosqueId":
+            MessageLookupByLibrary.simpleMessage("Voer Moske-ID in: "),
+        "facebook": MessageLookupByLibrary.simpleMessage("Facebook"),
+        "gpsError": MessageLookupByLibrary.simpleMessage(
+            "Geen toegang tot de huidige locatie van het apparaat, controleer of de GPS is ingeschakeld"),
+        "headerType": MessageLookupByLibrary.simpleMessage("Header type"),
+        "home": MessageLookupByLibrary.simpleMessage("Startpagina"),
+        "instagram": MessageLookupByLibrary.simpleMessage("Instagram"),
+        "installationGuide":
+            MessageLookupByLibrary.simpleMessage("Installatiehandleiding"),
+        "languages": MessageLookupByLibrary.simpleMessage("Leren"),
+        "leftButtonOption":
+            MessageLookupByLibrary.simpleMessage("Radio Knop opties (links)"),
+        "lightMode": MessageLookupByLibrary.simpleMessage("Lichte modus"),
+        "loadingAnimation":
+            MessageLookupByLibrary.simpleMessage("Animatie laden"),
+        "mawaqitDesc": MessageLookupByLibrary.simpleMessage(
+            "MAWAQIT biedt je een nieuwe manier om gebedstijden bij te houden en te beheren. We bieden inderdaad een end-to-end systeem dat moskeemanagers een online tool biedt die 24/24 uur beschikbaar is."),
+        "mawaqitWelcome":
+            MessageLookupByLibrary.simpleMessage("Welkom bij MAWAQIT"),
+        "messenger": MessageLookupByLibrary.simpleMessage("berichtgever"),
+        "missingMosqueId":
+            MessageLookupByLibrary.simpleMessage("Ontbrekende moske-ID"),
+        "mosque": MessageLookupByLibrary.simpleMessage("Moskee"),
+        "mosqueId": MessageLookupByLibrary.simpleMessage("Moske-ID "),
+        "mosqueIdIsNotValid": m0,
+        "mosqueInput": MessageLookupByLibrary.simpleMessage("Moskee input"),
+        "mosqueNameError":
+            MessageLookupByLibrary.simpleMessage("Ga de Moske-naam in"),
+        "mosqueNoMore":
+            MessageLookupByLibrary.simpleMessage("Niet meer resultaten"),
+        "mosqueNoResults":
+            MessageLookupByLibrary.simpleMessage("Geen resultaten"),
+        "navigationBarStyle":
+            MessageLookupByLibrary.simpleMessage("Stijl navigatiebalken"),
+        "networkStatus": MessageLookupByLibrary.simpleMessage("Netwerk Status"),
+        "no": MessageLookupByLibrary.simpleMessage("Nee"),
+        "noInternet":
+            MessageLookupByLibrary.simpleMessage("Geen internetverbinding"),
+        "notification": MessageLookupByLibrary.simpleMessage("Meldingen"),
+        "ok": MessageLookupByLibrary.simpleMessage("OK"),
+        "privacyPolicy": MessageLookupByLibrary.simpleMessage("Privacybeleid"),
+        "rate": MessageLookupByLibrary.simpleMessage("Beoordeel ons"),
+        "rightButtonOption":
+            MessageLookupByLibrary.simpleMessage("Radio Knop opties (links)"),
+        "searchForMosque":
+            MessageLookupByLibrary.simpleMessage("Zoek een moskee"),
+        "searchMosque": MessageLookupByLibrary.simpleMessage("Zoek een moskee"),
+        "selectMosqueId":
+            MessageLookupByLibrary.simpleMessage("Voer Moske-ID in"),
+        "selectWithMosqueId":
+            MessageLookupByLibrary.simpleMessage("Voer Moske-ID in"),
+        "share": MessageLookupByLibrary.simpleMessage("Delen"),
+        "skype": MessageLookupByLibrary.simpleMessage("Skype"),
+        "slugError": MessageLookupByLibrary.simpleMessage(
+            "Dit is geen geldige moskee slug"),
+        "snapchat": MessageLookupByLibrary.simpleMessage("Snapchat"),
+        "sureCloseApp": MessageLookupByLibrary.simpleMessage(
+            "Weet u zeker dat u de toepassing wilt afsluiten?"),
+        "termsOfService":
+            MessageLookupByLibrary.simpleMessage("Servicevoorwaarden"),
+        "tryAgain": MessageLookupByLibrary.simpleMessage("Probeer opnieuw"),
+        "twitter": MessageLookupByLibrary.simpleMessage("Twitter"),
+        "update": MessageLookupByLibrary.simpleMessage("Toepassing bijwerken"),
+        "whatsApp": MessageLookupByLibrary.simpleMessage("WhatsApp"),
+        "whoops": MessageLookupByLibrary.simpleMessage("Oeps!"),
+        "yes": MessageLookupByLibrary.simpleMessage("Ja"),
+        "youtube": MessageLookupByLibrary.simpleMessage("YouTube")
+      };
 }
--- lib/generated/intl/messages_ta.dart
+++ lib/generated/intl/messages_ta.dart
@@ -7,7 +7,8 @@
 // ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
-// ignore_for_file:unused_import, file_names
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
 
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
@@ -19,73 +20,103 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'ta';
 
-  static m0(mosqueId) => "${mosqueId} என்பது சரியான மசூதி ஐடி அல்ல";
+  static String m0(mosqueId) => "${mosqueId} என்பது சரியான மசூதி ஐடி அல்ல";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static _notInlinedMessages(_) => <String, Function> {
-    "about" : MessageLookupByLibrary.simpleMessage("இதைப் பற்றி"),
-    "appLang" : MessageLookupByLibrary.simpleMessage("ஆப் மொழி"),
-    "backToHomePage" : MessageLookupByLibrary.simpleMessage("Back to Homepage"),
-    "backendError" : MessageLookupByLibrary.simpleMessage("மன்னிக்கவும், எங்களால் சேவையகத்துடன் இணைக்க முடியவில்லை.\nதயவுசெய்து இணைய இணைப்பைச் சரிபார்க்கவும் அல்லது பிறகு முயற்சிக்கவும்."),
-    "cancel" : MessageLookupByLibrary.simpleMessage("ரத்துசெய்"),
-    "changeMosque" : MessageLookupByLibrary.simpleMessage("மசூதியை மாற்றவும்"),
-    "changeTheme" : MessageLookupByLibrary.simpleMessage("நிற அமைப்பை மாற்றவும்"),
-    "closeApp" : MessageLookupByLibrary.simpleMessage("பயன்பாட்டை மூடு"),
-    "colorGradient" : MessageLookupByLibrary.simpleMessage("Color Gradient"),
-    "colorSolid" : MessageLookupByLibrary.simpleMessage("Color Solid"),
-    "customizeYourOwnWay" : MessageLookupByLibrary.simpleMessage("Customize your own option"),
-    "darkMode" : MessageLookupByLibrary.simpleMessage("இருள் பயன்முறை"),
-    "descLang" : MessageLookupByLibrary.simpleMessage("தாங்கள் விரும்பும் மொழியினைத் தேர்வு செய்க."),
-    "doYouKnowMosqueId" : MessageLookupByLibrary.simpleMessage("உங்கள் நிறுவல் ஐடி அல்லது உங்கள் மசூதி ஐடி உங்களுக்குத் தெரியுமா?"),
-    "drawerDesc" : MessageLookupByLibrary.simpleMessage("Connecting Muslims to Mosques"),
-    "drawerTitle" : MessageLookupByLibrary.simpleMessage("MAWAQIT"),
-    "enterMosqueId" : MessageLookupByLibrary.simpleMessage("மசூதி ஐடியை உள்ளிடவும்:"),
-    "facebook" : MessageLookupByLibrary.simpleMessage("முகநூல்"),
-    "gpsError" : MessageLookupByLibrary.simpleMessage("சாதனத்தின் தற்போதைய இருப்பிடத்தை அணுக முடியவில்லை, உங்கள் சாதனத்தின் ஜிபிஎஸ் இயக்கப்பட்டுள்ளதா என்பதைச் சரிபார்க்கவும்"),
-    "headerType" : MessageLookupByLibrary.simpleMessage("Header type"),
-    "home" : MessageLookupByLibrary.simpleMessage("வீடு"),
-    "instagram" : MessageLookupByLibrary.simpleMessage("இன்ஸ்டாகிராம்"),
-    "installationGuide" : MessageLookupByLibrary.simpleMessage("நிறுவல் வழிகாட்டி"),
-    "languages" : MessageLookupByLibrary.simpleMessage("மொழி"),
-    "leftButtonOption" : MessageLookupByLibrary.simpleMessage("Radio Button Options (left)"),
-    "lightMode" : MessageLookupByLibrary.simpleMessage("வெளிர்ந்த பயன்முறை"),
-    "loadingAnimation" : MessageLookupByLibrary.simpleMessage("ஏற்றுதல் வட்ட அனிமேஷன்"),
-    "mawaqitDesc" : MessageLookupByLibrary.simpleMessage("MAWAQIT தொழுகை நேரத்தைக் கண்காணிக்கவும் நிர்வகிக்கவும் ஒரு புதிய வழியை உங்களுக்கு வழங்குகிறது, உண்மையில் நாங்கள் மசூதி மேலாளர்களுக்கு 24/24 மணிநேரம் கிடைக்கும் ஆன்லைன் கருவியை வழங்கும் இறுதி முதல் இறுதி முறையை வழங்குகிறோம்."),
-    "mawaqitWelcome" : MessageLookupByLibrary.simpleMessage("MAWAQIT க்கு வரவேற்கிறோம்"),
-    "messenger" : MessageLookupByLibrary.simpleMessage("தூதர்"),
-    "missingMosqueId" : MessageLookupByLibrary.simpleMessage("மசூதி ஐடி காணவில்லை"),
-    "mosque" : MessageLookupByLibrary.simpleMessage("மசூதி"),
-    "mosqueId" : MessageLookupByLibrary.simpleMessage("மசூதி ஐடி "),
-    "mosqueIdIsNotValid" : m0,
-    "mosqueInput" : MessageLookupByLibrary.simpleMessage("மசூதி உள்ளீடு"),
-    "mosqueNameError" : MessageLookupByLibrary.simpleMessage("மசூதியின் பெயரை உள்ளிடவும்"),
-    "mosqueNoMore" : MessageLookupByLibrary.simpleMessage("மேலும் முடிவுகள் இல்லை"),
-    "mosqueNoResults" : MessageLookupByLibrary.simpleMessage("முடிவுகள் இல்லை"),
-    "navigationBarStyle" : MessageLookupByLibrary.simpleMessage("Navigation bars style"),
-    "networkStatus" : MessageLookupByLibrary.simpleMessage("பிணைய நிலை"),
-    "no" : MessageLookupByLibrary.simpleMessage("இல்லை"),
-    "noInternet" : MessageLookupByLibrary.simpleMessage("இணைய இணைப்பு இல்லை"),
-    "notification" : MessageLookupByLibrary.simpleMessage("அறிவிப்புகள்"),
-    "ok" : MessageLookupByLibrary.simpleMessage("சரி"),
-    "privacyPolicy" : MessageLookupByLibrary.simpleMessage("தனியுரிமை கொள்கை"),
-    "rate" : MessageLookupByLibrary.simpleMessage("எங்களை மதிப்பிடுங்கள்"),
-    "rightButtonOption" : MessageLookupByLibrary.simpleMessage("Radio Button Options (right)"),
-    "searchForMosque" : MessageLookupByLibrary.simpleMessage("ஒரு மசூதியைத் தேடுங்கள்"),
-    "searchMosque" : MessageLookupByLibrary.simpleMessage("ஒரு மசூதியைத் தேடுங்கள்"),
-    "selectMosqueId" : MessageLookupByLibrary.simpleMessage("உங்கள் மசூதி ஐடியை உள்ளிடவும்"),
-    "selectWithMosqueId" : MessageLookupByLibrary.simpleMessage("உங்கள் மசூதி ஐடியை உள்ளிடவும்"),
-    "share" : MessageLookupByLibrary.simpleMessage("பகிர்"),
-    "skype" : MessageLookupByLibrary.simpleMessage("ஸ்கைப்"),
-    "slugError" : MessageLookupByLibrary.simpleMessage("செல்லுபடியாகும் மசூதி ஸ்லக் அல்லவா"),
-    "snapchat" : MessageLookupByLibrary.simpleMessage("Snapchat"),
-    "sureCloseApp" : MessageLookupByLibrary.simpleMessage("விண்ணப்பத்திலிருந்து நிச்சயமாக வெளியேற விரும்புகிறீர்களா?"),
-    "termsOfService" : MessageLookupByLibrary.simpleMessage("விதிமுறைகள்"),
-    "tryAgain" : MessageLookupByLibrary.simpleMessage("மீண்டும் முயற்சி"),
-    "twitter" : MessageLookupByLibrary.simpleMessage("ட்விட்டர்"),
-    "update" : MessageLookupByLibrary.simpleMessage("ஆப்-ஐ அப்டேட் செய்யவும்"),
-    "whatsApp" : MessageLookupByLibrary.simpleMessage("வாட்ஸ்அப்"),
-    "whoops" : MessageLookupByLibrary.simpleMessage("அச்சச்சோ!"),
-    "yes" : MessageLookupByLibrary.simpleMessage("ஆம்"),
-    "youtube" : MessageLookupByLibrary.simpleMessage("வலைஒளி")
-  };
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "about": MessageLookupByLibrary.simpleMessage("இதைப் பற்றி"),
+        "appLang": MessageLookupByLibrary.simpleMessage("ஆப் மொழி"),
+        "backToHomePage":
+            MessageLookupByLibrary.simpleMessage("Back to Homepage"),
+        "backendError": MessageLookupByLibrary.simpleMessage(
+            "மன்னிக்கவும், எங்களால் சேவையகத்துடன் இணைக்க முடியவில்லை.\nதயவுசெய்து இணைய இணைப்பைச் சரிபார்க்கவும் அல்லது பிறகு முயற்சிக்கவும்."),
+        "cancel": MessageLookupByLibrary.simpleMessage("ரத்துசெய்"),
+        "changeMosque":
+            MessageLookupByLibrary.simpleMessage("மசூதியை மாற்றவும்"),
+        "changeTheme":
+            MessageLookupByLibrary.simpleMessage("நிற அமைப்பை மாற்றவும்"),
+        "closeApp": MessageLookupByLibrary.simpleMessage("பயன்பாட்டை மூடு"),
+        "colorGradient": MessageLookupByLibrary.simpleMessage("Color Gradient"),
+        "colorSolid": MessageLookupByLibrary.simpleMessage("Color Solid"),
+        "customizeYourOwnWay":
+            MessageLookupByLibrary.simpleMessage("Customize your own option"),
+        "darkMode": MessageLookupByLibrary.simpleMessage("இருள் பயன்முறை"),
+        "descLang": MessageLookupByLibrary.simpleMessage(
+            "தாங்கள் விரும்பும் மொழியினைத் தேர்வு செய்க."),
+        "doYouKnowMosqueId": MessageLookupByLibrary.simpleMessage(
+            "உங்கள் நிறுவல் ஐடி அல்லது உங்கள் மசூதி ஐடி உங்களுக்குத் தெரியுமா?"),
+        "drawerDesc": MessageLookupByLibrary.simpleMessage(
+            "Connecting Muslims to Mosques"),
+        "drawerTitle": MessageLookupByLibrary.simpleMessage("MAWAQIT"),
+        "enterMosqueId":
+            MessageLookupByLibrary.simpleMessage("மசூதி ஐடியை உள்ளிடவும்:"),
+        "facebook": MessageLookupByLibrary.simpleMessage("முகநூல்"),
+        "gpsError": MessageLookupByLibrary.simpleMessage(
+            "சாதனத்தின் தற்போதைய இருப்பிடத்தை அணுக முடியவில்லை, உங்கள் சாதனத்தின் ஜிபிஎஸ் இயக்கப்பட்டுள்ளதா என்பதைச் சரிபார்க்கவும்"),
+        "headerType": MessageLookupByLibrary.simpleMessage("Header type"),
+        "home": MessageLookupByLibrary.simpleMessage("வீடு"),
+        "instagram": MessageLookupByLibrary.simpleMessage("இன்ஸ்டாகிராம்"),
+        "installationGuide":
+            MessageLookupByLibrary.simpleMessage("நிறுவல் வழிகாட்டி"),
+        "languages": MessageLookupByLibrary.simpleMessage("மொழி"),
+        "leftButtonOption":
+            MessageLookupByLibrary.simpleMessage("Radio Button Options (left)"),
+        "lightMode": MessageLookupByLibrary.simpleMessage("வெளிர்ந்த பயன்முறை"),
+        "loadingAnimation":
+            MessageLookupByLibrary.simpleMessage("ஏற்றுதல் வட்ட அனிமேஷன்"),
+        "mawaqitDesc": MessageLookupByLibrary.simpleMessage(
+            "MAWAQIT தொழுகை நேரத்தைக் கண்காணிக்கவும் நிர்வகிக்கவும் ஒரு புதிய வழியை உங்களுக்கு வழங்குகிறது, உண்மையில் நாங்கள் மசூதி மேலாளர்களுக்கு 24/24 மணிநேரம் கிடைக்கும் ஆன்லைன் கருவியை வழங்கும் இறுதி முதல் இறுதி முறையை வழங்குகிறோம்."),
+        "mawaqitWelcome":
+            MessageLookupByLibrary.simpleMessage("MAWAQIT க்கு வரவேற்கிறோம்"),
+        "messenger": MessageLookupByLibrary.simpleMessage("தூதர்"),
+        "missingMosqueId":
+            MessageLookupByLibrary.simpleMessage("மசூதி ஐடி காணவில்லை"),
+        "mosque": MessageLookupByLibrary.simpleMessage("மசூதி"),
+        "mosqueId": MessageLookupByLibrary.simpleMessage("மசூதி ஐடி "),
+        "mosqueIdIsNotValid": m0,
+        "mosqueInput": MessageLookupByLibrary.simpleMessage("மசூதி உள்ளீடு"),
+        "mosqueNameError":
+            MessageLookupByLibrary.simpleMessage("மசூதியின் பெயரை உள்ளிடவும்"),
+        "mosqueNoMore":
+            MessageLookupByLibrary.simpleMessage("மேலும் முடிவுகள் இல்லை"),
+        "mosqueNoResults":
+            MessageLookupByLibrary.simpleMessage("முடிவுகள் இல்லை"),
+        "navigationBarStyle":
+            MessageLookupByLibrary.simpleMessage("Navigation bars style"),
+        "networkStatus": MessageLookupByLibrary.simpleMessage("பிணைய நிலை"),
+        "no": MessageLookupByLibrary.simpleMessage("இல்லை"),
+        "noInternet":
+            MessageLookupByLibrary.simpleMessage("இணைய இணைப்பு இல்லை"),
+        "notification": MessageLookupByLibrary.simpleMessage("அறிவிப்புகள்"),
+        "ok": MessageLookupByLibrary.simpleMessage("சரி"),
+        "privacyPolicy":
+            MessageLookupByLibrary.simpleMessage("தனியுரிமை கொள்கை"),
+        "rate": MessageLookupByLibrary.simpleMessage("எங்களை மதிப்பிடுங்கள்"),
+        "rightButtonOption": MessageLookupByLibrary.simpleMessage(
+            "Radio Button Options (right)"),
+        "searchForMosque":
+            MessageLookupByLibrary.simpleMessage("ஒரு மசூதியைத் தேடுங்கள்"),
+        "searchMosque":
+            MessageLookupByLibrary.simpleMessage("ஒரு மசூதியைத் தேடுங்கள்"),
+        "selectMosqueId": MessageLookupByLibrary.simpleMessage(
+            "உங்கள் மசூதி ஐடியை உள்ளிடவும்"),
+        "selectWithMosqueId": MessageLookupByLibrary.simpleMessage(
+            "உங்கள் மசூதி ஐடியை உள்ளிடவும்"),
+        "share": MessageLookupByLibrary.simpleMessage("பகிர்"),
+        "skype": MessageLookupByLibrary.simpleMessage("ஸ்கைப்"),
+        "slugError": MessageLookupByLibrary.simpleMessage(
+            "செல்லுபடியாகும் மசூதி ஸ்லக் அல்லவா"),
+        "snapchat": MessageLookupByLibrary.simpleMessage("Snapchat"),
+        "sureCloseApp": MessageLookupByLibrary.simpleMessage(
+            "விண்ணப்பத்திலிருந்து நிச்சயமாக வெளியேற விரும்புகிறீர்களா?"),
+        "termsOfService": MessageLookupByLibrary.simpleMessage("விதிமுறைகள்"),
+        "tryAgain": MessageLookupByLibrary.simpleMessage("மீண்டும் முயற்சி"),
+        "twitter": MessageLookupByLibrary.simpleMessage("ட்விட்டர்"),
+        "update":
+            MessageLookupByLibrary.simpleMessage("ஆப்-ஐ அப்டேட் செய்யவும்"),
+        "whatsApp": MessageLookupByLibrary.simpleMessage("வாட்ஸ்அப்"),
+        "whoops": MessageLookupByLibrary.simpleMessage("அச்சச்சோ!"),
+        "yes": MessageLookupByLibrary.simpleMessage("ஆம்"),
+        "youtube": MessageLookupByLibrary.simpleMessage("வலைஒளி")
+      };
 }
