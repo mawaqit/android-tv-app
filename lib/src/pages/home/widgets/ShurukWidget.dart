da2b9122547323ed3e20ff66f48eb6b62d8049a8
AnasElbattra <anaselbattra1@gmail.com>
2023-01-27 18:28:15 +0200

    add Shuruk in time

--- lib/src/pages/home/widgets/ShurukWidget.dart
+++ lib/src/pages/home/widgets/ShurukWidget.dart
@@ -12,34 +12,34 @@ class ShurukWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final mosqueProvider = context.read<MosqueManager>();
 
-    return      mosqueProvider.showImsak
+    return mosqueProvider.showImsak
         ? Center(
-      child: SalahItemWidget(
-        title: S.of(context).imsak,
-        time: mosqueProvider.imsak,
-        removeBackground: true,
-        withDivider: false,
-      ),
-    )
+            child: SalahItemWidget(
+              title: S.of(context).imsak,
+              time: mosqueProvider.imsak,
+              removeBackground: true,
+              withDivider: false,
+            ),
+          )
         : mosqueProvider.showEid
-        ? Center(
-      child: SalahItemWidget(
-        title: "Salat El Eid",
-        iqama: mosqueProvider.times!.aidPrayerTime2,
-        time: mosqueProvider.times!.aidPrayerTime ?? "",
-        removeBackground: false,
-        withDivider: mosqueProvider.times!.aidPrayerTime2 != null,
-        active: true,
-      ),
-    )
-        : Center(
-      child: SalahItemWidget(
-        title: S.of(context).shuruk,
-        time: mosqueProvider.times!.shuruq ?? "",
-        removeBackground: true,
-        withDivider: false,
-        active: mosqueProvider.activateShroukItem,
-      ),
-    );
+            ? Center(
+                child: SalahItemWidget(
+                  title: "Salat El Eid",
+                  iqama: mosqueProvider.times!.aidPrayerTime2,
+                  time: mosqueProvider.times!.aidPrayerTime ?? "",
+                  removeBackground: false,
+                  withDivider: mosqueProvider.times!.aidPrayerTime2 != null,
+                  active: true,
+                ),
+              )
+            : Center(
+                child: SalahItemWidget(
+                  title: S.of(context).shuruk,
+                  time: mosqueProvider.times!.shuruq ?? "",
+                  removeBackground: true,
+                  withDivider: false,
+                  active: mosqueProvider.activateShroukItem,
+                ),
+              );
   }
 }
