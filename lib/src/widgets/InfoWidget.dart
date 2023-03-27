284819bcd1d0aa477f0ae55501515ff5b33c9d8f
ismaelAbogabal <gabalismael@gmail.com>
2023-03-27 14:27:12 +0200

    [discontinued[ package info replaced with package_info_plus

--- lib/src/widgets/InfoWidget.dart
+++ lib/src/widgets/InfoWidget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class VersionWidget extends StatelessWidget {
   const VersionWidget({Key? key, this.style, this.textAlign}) : super(key: key);
