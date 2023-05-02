cc6f3dbaa62181e9a57e49142288d34e6a3a3557
ismaelAbogabal <gabalismael@gmail.com>
2023-05-02 12:22:00 +0200

    allow developer to test staging backend

--- lib/src/pages/ErrorScreen.dart
+++ lib/src/pages/ErrorScreen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:mawaqit/src/elements/RaisedGradientButton.dart';
+import 'package:mawaqit/src/helpers/AppRouter.dart';
 import 'package:mawaqit/src/helpers/HexColor.dart';
+import 'package:mawaqit/src/services/user_preferences_manager.dart';
 import 'package:mawaqit/src/widgets/InfoWidget.dart';
+import 'package:provider/provider.dart';
 
 import '../../i18n/l10n.dart';
 
@@ -14,6 +17,7 @@ class ErrorScreen extends StatelessWidget {
     required this.image,
     this.onTryAgain,
     this.tryAgainText,
+    this.allowDisableStaging = true,
   }) : super(key: key);
 
   final String title;
@@ -21,9 +25,12 @@ class ErrorScreen extends StatelessWidget {
   final String image;
   final VoidCallback? onTryAgain;
   final String? tryAgainText;
+  final bool allowDisableStaging;
 
   @override
   Widget build(BuildContext context) {
+    final userPrefs = Provider.of<UserPreferencesManager>(context);
+
     return Scaffold(
       body: SizedBox(
         width: double.infinity,
@@ -49,10 +56,7 @@ class ErrorScreen extends StatelessWidget {
             SizedBox(height: 10),
             Text(
               title,
-              style: TextStyle(
-                  color: Colors.white70,
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold),
+              style: TextStyle(color: Colors.white70, fontSize: 40.0, fontWeight: FontWeight.bold),
             ),
             Text(
               description,
@@ -63,17 +67,27 @@ class ErrorScreen extends StatelessWidget {
               autoFocus: true,
               child: Text(
                 tryAgainText ?? S.of(context).tryAgain,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
+                style: TextStyle(color: Colors.white, fontSize: 18.0, fontWeight: FontWeight.bold),
               ),
               width: 250,
-              gradient: LinearGradient(
-                  colors: <Color>[HexColor("#391e61"), HexColor("#490094")]),
+              gradient: LinearGradient(colors: <Color>[HexColor("#391e61"), HexColor("#490094")]),
               onPressed: onTryAgain,
             ),
+            SizedBox(height: 10),
+            if (allowDisableStaging && userPrefs.forceStaging)
+              RaisedGradientButton(
+                autoFocus: true,
+                child: Text(
+                  tryAgainText ?? S.of(context).disableStaging,
+                  style: TextStyle(color: Colors.white, fontSize: 18.0, fontWeight: FontWeight.bold),
+                ),
+                width: 250,
+                gradient: LinearGradient(colors: <Color>[HexColor("#391e61"), HexColor("#490094")]),
+                onPressed: () {
+                  userPrefs.forceStaging = false;
+                  onTryAgain?.call();
+                },
+              ),
             Spacer(),
             Opacity(child: VersionWidget(), opacity: .3),
             SizedBox(height: 10),
