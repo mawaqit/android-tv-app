7d7115a120487d0cf31e1a2f5697d65ee01ee0ed
ismaelAbogabal <gabalismael@gmail.com>
2023-05-03 12:33:21 +0200

    add Mawaqit cached image

---
+++ lib/src/mawaqit_image/mawaqit_network_image.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MawaqitNetworkImage extends Image {
+  MawaqitNetworkImage({
+    super.alignment,
+    super.repeat,
+    super.matchTextDirection = false,
+    super.gaplessPlayback = false,
+    super.excludeFromSemantics = false,
+    super.semanticLabel,
+    super.width,
+    super.height,
+    super.color,
+    super.fit,
+    super.centerSlice,
+    super.colorBlendMode,
+    super.filterQuality = FilterQuality.low,
+    super.frameBuilder,
+    super.loadingBuilder,
+    super.errorBuilder,
+    super.isAntiAlias = false,
+    super.key,
+    super.opacity,
+  }) : super(image: null);
+}
