d077ef7dd5d091dfbb97bc87addec8df5d58a605
ismaelAbogabal <63953869+ismaelAbogabal@users.noreply.github.com>
2023-06-14 10:16:44 +0300

    custom month names on TN, DZ

--- lib/src/helpers/DateUtils.dart
+++ lib/src/helpers/DateUtils.dart
@@ -1,13 +1,31 @@
 import 'package:intl/intl.dart';
 import 'package:mawaqit/src/helpers/StringUtils.dart';
 
+const tunisMonthNames = [
+  'جانفي',
+  'فيفري',
+  'مارس',
+  'أفريل',
+  'ماي',
+  'جوان',
+  'جويلية',
+  'أوت',
+  'سبتمبر',
+  'أكتوبر',
+  'نوفمبر',
+  'ديسمبر'
+];
+
 extension MawaqitDateUtils on DateTime {
-  String formatIntoMawaqitFormat({
-    String local = 'en',
-  }) {
+  String formatIntoMawaqitFormat({String local = 'en'}) {
     var formatter = local == 'ar' || local == 'fr'
-        ? DateFormat('EEEE, dd MMMM, yyyy' , local)
-        : DateFormat('EEEE, MMMM dd, yyyy' , local);
+        ? DateFormat('EEEE, dd MMMM, yyyy', local)
+        : DateFormat('EEEE, MMMM dd, yyyy', local);
+
+    if (local.toUpperCase() == 'AR_TN' || local.toUpperCase() == 'DZ') {
+      formatter.dateSymbols.MONTHS = tunisMonthNames;
+    }
+
     formatter.useNativeDigits = false;
     return formatter.format(this).capitalizeFirstOfEach();
   }
