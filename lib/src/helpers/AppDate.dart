5684f922f25dfaf7afe3ee7ad8a55ea1f29c916f
ismaelAbogabal <63953869+ismaelAbogabal@users.noreply.github.com>
2023-09-16 18:01:56 +0300

    support hot restart for app date

--- lib/src/helpers/AppDate.dart
+++ lib/src/helpers/AppDate.dart
@@ -5,7 +5,7 @@ class AppDateTime {
   AppDateTime._();
 
   /// for debug purpose to be able to skip the time forward or backward
-  static Duration difference = Duration(hours: -6, minutes: 30);
+  static Duration get difference => Duration(hours: -6, minutes: 30);
 
   static DateTime now() => kDebugMode ? DateTime.now().add(difference) : DateTime.now();
 }
