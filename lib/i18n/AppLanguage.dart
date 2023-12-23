ea64f888ec2333f53650e13809e1bf13c82ea145
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2023-12-24 01:52:57 +0200

    feat(app-language): add combinedLanguage for handle bilingual and getHadithLanguage shared preference

--- lib/i18n/AppLanguage.dart
+++ lib/i18n/AppLanguage.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:global_configuration/global_configuration.dart';
 import 'package:mawaqit/i18n/l10n.dart';
@@ -6,10 +7,28 @@ import 'package:mawaqit/src/helpers/AnalyticsWrapper.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppLanguage extends ChangeNotifier {
+  /// [kHadithLanguage] key stored in the shared preference
+  static const  String kHadithLanguage = 'hadith_language';
+  String _hadithLanguage = "";
   Locale _appLocale = Locale('en', '');
 
   String languageName(String languageCode) => Config.isoLang[languageCode]?['nativeName'] ?? languageCode;
 
+  /// return the language name of the combined language
+  /// Example: 'en-ar' will be 'English & Arabic'
+  String combinedLanguageName(String languageCode) {
+    List<String> codes = languageCode.split('_');
+    // Check if we have a combined language code
+    if (codes.length == 2) {
+      // Retrieve individual language names and combine them
+      String firstLanguage = Config.isoLang[codes[0]]?['nativeName'] ?? codes[0];
+      String secondLanguage = Config.isoLang[codes[1]]?['nativeName'] ?? codes[1];
+      return '$firstLanguage & $secondLanguage';
+    } else {
+      // Return the language name
+      return Config.isoLang[languageCode]?['nativeName'] ?? languageCode;
+    }
+  }
   String get currentLanguageName => languageName(_appLocale.languageCode);
 
   Locale get appLocal => _appLocale;
@@ -53,4 +72,49 @@ class AppLanguage extends ChangeNotifier {
   bool isArabic() {
     return _appLocale.languageCode == "ar" || _appLocale.languageCode == "ur";
   }
+
+  Map<String, String Function(BuildContext)> hadithLocalizedLanguage = {
+    'en': (context) => S.of(context).en,
+    'fr': (context) => S.of(context).fr,
+    'ar': (context) => S.of(context).ar,
+    'tr': (context) => S.of(context).tr,
+    'de': (context) => S.of(context).de,
+    'es': (context) => S.of(context).es,
+    'pt': (context) => S.of(context).pt,
+    'nl': (context) => S.of(context).nl,
+    'fr_ar': (context) => S.of(context).fr_ar,
+    'en_ar': (context) => S.of(context).en_ar,
+    'de_ar': (context) => S.of(context).de_ar,
+    'ta_ar': (context) => S.of(context).ta_ar,
+    'tr_ar': (context) => S.of(context).tr_ar,
+    'es_ar': (context) => S.of(context).es_ar,
+    'pt_ar': (context) => S.of(context).pt_ar,
+    'nl_ar': (context) => S.of(context).nl_ar,
+  };
+
+
+  /// set the language of the hadith in shared preference
+  Future<void> setHadithLanguage(String language) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    _hadithLanguage = language;
+    await prefs.setString(kHadithLanguage, language);
+    notifyListeners();
+  }
+
+  /// get the language of the hadith from shared preference
+  /// if there is no language saved, return the api default language
+  Future<String?> getHadithLanguage() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? hadithLanguage = prefs.getString(kHadithLanguage);
+    if(hadithLanguage != null) {
+      _hadithLanguage = hadithLanguage;
+      notifyListeners();
+      return hadithLanguage;
+    }
+    notifyListeners();
+    return null;
+  }
+
+  /// getters for the hadith language
+  String get hadithLanguage => _hadithLanguage;
 }
