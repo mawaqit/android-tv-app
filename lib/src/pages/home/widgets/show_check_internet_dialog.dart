93c1005b11006712c594b2689c24f2016ad0c80e
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2023-12-26 20:01:42 +0200

    feat: add dialog to retry connect if the device is not connected

---
+++ lib/src/pages/home/widgets/show_check_internet_dialog.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../../../../i18n/l10n.dart';
+
+void showCheckInternetDialog({
+  required BuildContext context,
+  required VoidCallback onRetry,
+  required String title,
+  required String content,
+})  {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      final String retry = S.of(context).retry;
+      return AlertDialog(
+        title: Text(title),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text(content),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: Text(retry),
+            onPressed: () {
+              Navigator.of(context).pop(); // Close the dialog
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
