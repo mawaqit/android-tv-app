74652c0e5435262d45ac3413d6ed6f10a15591ea
Ghassen Ben Zahra <ghassen.benzahra@gmail.com>
2024-01-31 22:58:03 +0100

    readd enable/disable debug menu from about menu & change the tap on logo

--- lib/src/pages/home/widgets/HomeLogoVersion.dart
+++ lib/src/pages/home/widgets/HomeLogoVersion.dart
@@ -3,9 +3,19 @@ import 'package:flutter_svg/svg.dart';
 import 'package:mawaqit/const/resource.dart';
 import 'package:mawaqit/src/helpers/RelativeSizes.dart';
 import 'package:mawaqit/src/widgets/InfoWidget.dart';
+import 'package:provider/provider.dart';
 
-class HomeLogoVersion extends StatelessWidget {
+import '../../../services/user_preferences_manager.dart';
+
+class HomeLogoVersion extends StatefulWidget {
   const HomeLogoVersion({Key? key}) : super(key: key);
+
+  @override
+  _HomeLogoVersionState createState() => _HomeLogoVersionState();
+}
+
+class _HomeLogoVersionState extends State<HomeLogoVersion> {
+  int tapCount = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -13,15 +23,46 @@ class HomeLogoVersion extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          SvgPicture.asset(
-            R.ASSETS_SVG_MAWAQIT_LOGO_TEXT_LIGHT_SVG,
-            height: 3.8.vwr,
+          GestureDetector(
+            onTap: () {
+              if (Provider.of<UserPreferencesManager>(context, listen: false)
+                  .developerModeEnabled) {
+                // Deactivate debug menu
+                Provider.of<UserPreferencesManager>(context, listen: false)
+                    .developerModeEnabled = false;
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                        "You have deactivated the Abogabal secret menu 😎💪 رائع! لقد قمت بإلغاء تنشيط قائمة أبو جبل السرية"),
+                  ),
+                );
+              } else {
+                tapCount++;
+                if (tapCount >= 7) {
+                  Provider.of<UserPreferencesManager>(context, listen: false)
+                      .developerModeEnabled = true;
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        "You have activated the Abogabal secret menu 😎💪 رائع! لقد قمت بتنشيط قائمة أبو جبل السرية",
+                      ),
+                    ),
+                  );
+                  tapCount = 0; // Reset tapCount after activation
+                }
+              }
+            },
+            child: SvgPicture.asset(
+              R.ASSETS_SVG_MAWAQIT_LOGO_TEXT_LIGHT_SVG,
+              height: 3.8.vwr,
+            ),
           ),
           Align(
             heightFactor: .5,
             alignment: Alignment(.5, 0),
             child: Container(
-              padding: EdgeInsets.symmetric(horizontal: .5.vwr, vertical: .4.vh),
+              padding:
+                  EdgeInsets.symmetric(horizontal: .5.vwr, vertical: .4.vh),
               decoration: BoxDecoration(
                 color: Theme.of(context).primaryColor,
                 borderRadius: BorderRadius.vertical(
--- lib/src/widgets/InfoWidget.dart
+++ lib/src/widgets/InfoWidget.dart
@@ -1,61 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
-import 'package:provider/provider.dart';
 
-import '../services/user_preferences_manager.dart';
-
-class VersionWidget extends StatefulWidget {
+class VersionWidget extends StatelessWidget {
   const VersionWidget({Key? key, this.style, this.textAlign}) : super(key: key);
 
   final TextStyle? style;
   final TextAlign? textAlign;
 
   @override
-  _VersionWidgetState createState() => _VersionWidgetState();
-}
-
-class _VersionWidgetState extends State<VersionWidget> {
-  int tapCount = 0;
-
-  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-onTap: () {
-        setState(() {
-          if (context.read<UserPreferencesManager>().developerModeEnabled) {
-            // Deactivate debug menu
-            context.read<UserPreferencesManager>().developerModeEnabled = false;
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(
-                    "You have desactivated the Abogabal secret menu 😎💪 رائع! لقد قمت بإلغاء تنشيط قائمة أبو جبل السرية"),
-              ),
-            );
-          } else {
-            tapCount++;
-            if (tapCount >= 7) {
-              context.read<UserPreferencesManager>().developerModeEnabled =
-                  true;
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text(
-                    "You have activated the Abogabal secret menu 😎💪 رائع! لقد قمت بتنشيط قائمة أبو جبل السرية",
-                  ),
-                ),
-              );
-              tapCount = 0; // Reset tapCount after activation
-            }
-          }
-        });
-      },
-
-      child: FutureBuilder<PackageInfo>(
-        future: PackageInfo.fromPlatform(),
-        builder: (context, snapshot) => Text(
-          "v${snapshot.data?.version.replaceAll('-tv', '')}-${snapshot.data?.buildNumber}",
-          style: widget.style,
-          textAlign: widget.textAlign,
-        ),
+    return FutureBuilder<PackageInfo>(
+      future: PackageInfo.fromPlatform(),
+      builder: (context, snapshot) => Text(
+        "v${snapshot.data?.version.replaceAll('-tv', '')}-${snapshot.data?.buildNumber}",
+        style: style,
+        textAlign: textAlign,
       ),
     );
   }
