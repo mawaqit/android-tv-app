4c0f79266ddccc06aaed4b76864191e65bed71b0
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2024-03-22 12:07:03 +0200

    fix: Background image not persisting after box force restart in both offline & online mode (#1071)
    * feat(constants): add CacheKey class for cache key constants
    
    * refactor(mosque_background_screen): enhance background image handling
    
    - Replace direct mosque background image fetching with `CachedNetworkImage` for efficient loading and caching.
    - Introduce a method `_buildBackgroundDecoration` to handle different background types (color or image) and conditions.
    - Use the newly added `CacheKey.kMosqueBackgroundScreen` for caching the background image.

--- lib/src/pages/home/widgets/mosque_background_screen.dart
+++ lib/src/pages/home/widgets/mosque_background_screen.dart
@@ -1,76 +1,126 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mawaqit/src/helpers/AppRouter.dart';
 import 'package:mawaqit/src/helpers/HexColor.dart';
-import 'package:mawaqit/src/mawaqit_image/mawaqit_image_cache.dart';
-import 'package:mawaqit/src/pages/home/OfflineHomeScreen.dart';
 import 'package:mawaqit/src/services/mosque_manager.dart';
 import 'package:mawaqit/src/widgets/MawaqitDrawer.dart';
 import 'package:provider/provider.dart';
 
-/// used to show the background of the mosque
-/// used with the sunscreens
-/// in case you need to show sub screen without the [OfflineHomeScreen]
-class MosqueBackgroundScreen extends StatefulWidget {
-  MosqueBackgroundScreen({Key? key, required this.child}) : super(key: key);
+import '../../../const/constants.dart';
 
+class MosqueBackgroundScreen extends StatefulWidget {
   final Widget child;
+
+  const MosqueBackgroundScreen({Key? key, required this.child}) : super(key: key);
 
   @override
   State<MosqueBackgroundScreen> createState() => _MosqueBackgroundScreenState();
 }
 
 class _MosqueBackgroundScreenState extends State<MosqueBackgroundScreen> {
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  late FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final mosqueProvider = context.watch<MosqueManager>();
-    final mosqueConfig = mosqueProvider.mosqueConfig;
-
-    if (!mosqueProvider.loaded) return SizedBox();
-
+    if (!mosqueProvider.loaded) return const SizedBox();
     return RawKeyboardListener(
-       focusNode: FocusNode(),
-       onKey: (event) {
-         if (event is RawKeyDownEvent) {
-           if (event.logicalKey == LogicalKeyboardKey.arrowDown ||
-               event.logicalKey == LogicalKeyboardKey.arrowUp ||
-               event.logicalKey == LogicalKeyboardKey.arrowLeft ||
-               event.logicalKey == LogicalKeyboardKey.arrowRight) {
-             _scaffoldKey.currentState?.openDrawer();
-           }
-         }
-       },
+      focusNode: _focusNode,
+      onKey: (event) {
+        if (event is RawKeyDownEvent && event.isArrow) {
+          _scaffoldKey.currentState?.openDrawer();
+        }
+      },
       child: Focus(
         autofocus: true,
         child: Scaffold(
           key: _scaffoldKey,
           drawer: MawaqitDrawer(goHome: () => AppRouter.popAll()),
-          body: Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: mosqueConfig!.backgroundType == "color"
-                ? BoxDecoration(color: HexColor(mosqueConfig.backgroundColor))
-                : BoxDecoration(
-                    image: DecorationImage(
-                      image: mosqueConfig.backgroundMotif == "0"
-                          ? MawaqitNetworkImageProvider(mosqueProvider.mosque?.interiorPicture ?? "")
-                          : mosqueConfig.backgroundMotif == "-1"
-                              ? MawaqitNetworkImageProvider(mosqueProvider.mosque?.exteriorPicture ?? "")
-                              : MawaqitNetworkImageProvider(mosqueProvider.mosqueConfig!.motifUrl),
-                      fit: BoxFit.cover,
-                      colorFilter: ColorFilter.mode(
-                        Colors.black.withOpacity(.4),
-                        BlendMode.srcOver,
-                      ),
-                      onError: (exception, stackTrace) {},
-                    ),
-                  ),
-            child: RepaintBoundary(child: widget.child),
-          ),
+          body: _buildBackgroundDecoration(mosqueProvider),
         ),
       ),
     );
   }
+
+  Widget _buildBackgroundDecoration(MosqueManager mosqueProvider) {
+    final mosqueConfig = mosqueProvider.mosqueConfig!;
+    if (mosqueConfig.backgroundType == "color") {
+      return Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: HexColor(mosqueConfig.backgroundColor),
+        child: RepaintBoundary(child: widget.child),
+      );
+    } else {
+      String imageUrl = '';
+      switch (mosqueConfig.backgroundMotif) {
+        case "0":
+          imageUrl = mosqueProvider.mosque?.interiorPicture ?? "";
+          break;
+        case "-1":
+          imageUrl = mosqueProvider.mosque?.exteriorPicture ?? "";
+          break;
+        default:
+          imageUrl = mosqueConfig.motifUrl;
+      }
+
+      return CachedNetworkImage(
+        cacheKey: CacheKey.kMosqueBackgroundScreen,
+        imageUrl: imageUrl,
+        imageBuilder: (context, imageProvider) {
+          return Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: RepaintBoundary(child: widget.child),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: imageProvider,
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(Colors.black.withOpacity(.4), BlendMode.srcOver),
+              ),
+            ),
+          );
+        },
+        placeholder: (context, url) {
+          return Center(
+            child: SizedBox(
+              width: 40,
+              height: 40,
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor), // Green color
+              ),
+            ),
+          );
+        },
+        errorWidget: (context, url, error) => Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.black,
+          child: RepaintBoundary(child: widget.child),
+        ),
+      );
+    }
+  }
+}
+
+extension on RawKeyDownEvent {
+  bool get isArrow =>
+      logicalKey == LogicalKeyboardKey.arrowDown ||
+      logicalKey == LogicalKeyboardKey.arrowUp ||
+      logicalKey == LogicalKeyboardKey.arrowLeft ||
+      logicalKey == LogicalKeyboardKey.arrowRight;
 }
