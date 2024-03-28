c71968bebc10570352260f6d2a6c33db1412c2eb
Ghassen Ben Zahra <ghassen.benzahra@gmail.com>
2024-03-28 12:36:25 +0100

    add restart app option on developer menu

--- android/app/src/main/kotlin/com/flyweb/MainActivity.kt
+++ android/app/src/main/kotlin/com/flyweb/MainActivity.kt
@@ -8,6 +8,9 @@ import io.flutter.embedding.engine.dart.DartExecutor.DartEntrypoint
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
+import java.io.IOException
+import android.app.Activity
+import android.content.Intent
 
 class MainActivity : FlutterActivity() {
     private val CHANNEL = "nativeMethodsChannel"
@@ -20,6 +23,10 @@ class MainActivity : FlutterActivity() {
                 val packageName = call.argument<String>("packageName")
                 val isInstalled = isPackageInstalled(packageName)
                 result.success(isInstalled)
+            } else  if (call.method == "clearAppData") {
+             
+                val isSuccess = clearDataRestart()
+                result.success(isSuccess)
             } else {
                 result.notImplemented()
             }
@@ -35,4 +42,35 @@ class MainActivity : FlutterActivity() {
             false
         }
     }
+private fun clearDataRestart(): Boolean {
+    try {
+        val processBuilder = ProcessBuilder()
+        processBuilder.command("sh", "-c", """
+            pm clear com.mawaqit.androidtv
+        """.trimIndent())
+        val process = processBuilder.start()
+        val exitCode = process.waitFor()
+        if (exitCode == 0) {
+            triggerRestart(this)
+            return true
+        }
+        return false
+    } catch (e: IOException) {
+        e.printStackTrace()
+        return false
+    } catch (e: InterruptedException) {
+        e.printStackTrace()
+        return false
+    }
+}
+
+private fun triggerRestart(context: Activity) {
+    val intent = Intent(context, MainActivity::class.java)
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    context.startActivity(intent)
+    if (context is Activity) {
+        (context as Activity).finish()
+    }
+    Runtime.getRuntime().exit(0)
+}
 }
