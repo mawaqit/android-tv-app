277e52c1cf597a83ddc683cabe8cfbb28cfeae1a
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2024-03-31 05:40:28 +0200

    fix (MainActivity.kt): Remove else branch, reformat code
    - Removed redundant else branches in `configureFlutterEngine` method for error logic flow.
    - Reformatted code for consistency with Kotlin coding standards, improving readability and maintainability.

--- android/app/src/main/kotlin/com/flyweb/MainActivity.kt
+++ android/app/src/main/kotlin/com/flyweb/MainActivity.kt
@@ -14,67 +14,63 @@ import android.content.Intent
 
 
 class MainActivity : FlutterActivity() {
-    private val CHANNEL = "nativeMethodsChannel"
+  private val CHANNEL = "nativeMethodsChannel"
 
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-            if (call.method == "isPackageInstalled") {
-                val packageName = call.argument<String>("packageName")
-                val isInstalled = isPackageInstalled(packageName)
-                result.success(isInstalled)
-            } else  if (call.method == "clearAppData") {
-             
-                val isSuccess = clearDataRestart()
-                result.success(isSuccess)
-            } else {
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    GeneratedPluginRegistrant.registerWith(flutterEngine)
+    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+      if (call.method == "isPackageInstalled") {
+        val packageName = call.argument<String>("packageName")
+        val isInstalled = isPackageInstalled(packageName)
+        result.success(isInstalled)
+      } else if (call.method == "clearAppData") {
 
-            }  else if (call.method == "hasSystemFeature") {
-                val feature = call.argument<String>("feature")
- if (feature != null) {
-        val pm = applicationContext.packageManager
-        val hasFeature = pm.hasSystemFeature(feature)
-        result.success(hasFeature)
-    } else {
-        result.error("InvalidArgument", "Feature argument is null", null)
-    }
-            }
-            
-            else {
-                result.notImplemented()
-            }
+        val isSuccess = clearDataRestart()
+        result.success(isSuccess)
+      } else if (call.method == "hasSystemFeature") {
+        val feature = call.argument<String>("feature")
+        if (feature != null) {
+          val pm = applicationContext.packageManager
+          val hasFeature = pm.hasSystemFeature(feature)
+          result.success(hasFeature)
+        } else {
+          result.error("InvalidArgument", "Feature argument is null", null)
         }
+      } else {
+        result.notImplemented()
+      }
     }
+  }
 
-    private fun isPackageInstalled(packageName: String?): Boolean {
-        val packageManager = applicationContext.packageManager
-        return try {
-            packageManager.getPackageInfo(packageName!!, PackageManager.GET_ACTIVITIES)
-            true
-        } catch (e: PackageManager.NameNotFoundException) {
-            false
-        }
+  private fun isPackageInstalled(packageName: String?): Boolean {
+    val packageManager = applicationContext.packageManager
+    return try {
+      packageManager.getPackageInfo(packageName!!, PackageManager.GET_ACTIVITIES)
+      true
+    } catch (e: PackageManager.NameNotFoundException) {
+      false
     }
-private fun clearDataRestart(): Boolean {
+  }
+
+  private fun clearDataRestart(): Boolean {
     try {
-        val processBuilder = ProcessBuilder()
-        processBuilder.command("sh", "-c", """
+      val processBuilder = ProcessBuilder()
+      processBuilder.command("sh", "-c", """
             pm clear com.mawaqit.androidtv
         """.trimIndent())
-        val process = processBuilder.start()
-        val exitCode = process.waitFor()
-        if (exitCode == 0) {
-            return true
-        }
-        return false
+      val process = processBuilder.start()
+      val exitCode = process.waitFor()
+      if (exitCode == 0) {
+        return true
+      }
+      return false
     } catch (e: IOException) {
-        e.printStackTrace()
-        return false
+      e.printStackTrace()
+      return false
     } catch (e: InterruptedException) {
-        e.printStackTrace()
-        return false
+      e.printStackTrace()
+      return false
     }
-}
-
+  }
 }
