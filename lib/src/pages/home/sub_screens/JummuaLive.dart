953033c99586e37aea326195a2b842364435908c
Ghassen Ben Zahra <ghassen.benzahra@gmail.com>
2024-04-03 16:38:14 +0200

    Add condition to fallback to JumuaHadithSubscreen in offline mode

--- lib/src/pages/home/sub_screens/JummuaLive.dart
+++ lib/src/pages/home/sub_screens/JummuaLive.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:mawaqit/src/pages/home/sub_screens/JumuaHadithSubScreen.dart';
-import 'package:mawaqit/src/services/user_preferences_manager.dart';
-import 'package:mawaqit/src/widgets/mawaqit_youtube_palyer.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mawaqit/i18n/l10n.dart';
+import 'package:mawaqit/src/helpers/RelativeSizes.dart';
+import 'package:mawaqit/src/models/address_model.dart';
+import 'package:mawaqit/src/services/mosque_manager.dart';
+import 'package:mawaqit/src/themes/UIShadows.dart';
 import 'package:provider/provider.dart';
 
-import '../../../services/mosque_manager.dart';
+import '../../../helpers/connectivity_provider.dart';
+import '../../../services/user_preferences_manager.dart';
+import '../../../widgets/mawaqit_youtube_palyer.dart';
+import 'JumuaHadithSubScreen.dart';
 
-class JummuaLive extends StatefulWidget {
+class JummuaLive extends ConsumerStatefulWidget {
   const JummuaLive({
     Key? key,
     this.onDone,
@@ -15,10 +21,10 @@ class JummuaLive extends StatefulWidget {
   final VoidCallback? onDone;
 
   @override
-  State<JummuaLive> createState() => _JummuaLiveState();
+  ConsumerState createState() => _JummuaLiveState();
 }
 
-class _JummuaLiveState extends State<JummuaLive> {
+class _JummuaLiveState extends ConsumerState<JummuaLive> {
   /// invalid channel id
   bool invalidStreamUrl = false;
 
@@ -33,21 +39,34 @@ class _JummuaLiveState extends State<JummuaLive> {
   Widget build(BuildContext context) {
     final mosqueManager = context.read<MosqueManager>();
     final userPrefs = context.watch<UserPreferencesManager>();
+    final connectivity = ref.watch(connectivityProvider);
 
     /// disable live stream in mosque primary screen
     final jumuaaDisableInMosque = !userPrefs.isSecondaryScreen && mosqueManager.typeIsMosque;
 
-    if (invalidStreamUrl || mosqueManager.mosque?.streamUrl == null || jumuaaDisableInMosque) {
+    return switch (connectivity) {
+      AsyncData(:final value) =>
+        switchStreamWidget(value, mosqueManager, jumuaaDisableInMosque),
+      _ => const CircularProgressIndicator(),
+    };
+  }
+
+  Widget switchStreamWidget(ConnectivityStatus connectivityStatus,
+      MosqueManager mosqueManager, bool jumuaaDisableInMosque) {
+    if (invalidStreamUrl ||
+        mosqueManager.mosque?.streamUrl == null ||
+        jumuaaDisableInMosque ||
+        connectivityStatus == ConnectivityStatus.disconnected) {
       if (mosqueManager.mosqueConfig!.jumuaDhikrReminderEnabled == true) return JumuaHadithSubScreen(onDone: widget.onDone);
 
       return Scaffold(backgroundColor: Colors.black);
+    } else {
+      return MawaqitYoutubePlayer(
+        channelId: mosqueManager.mosque!.streamUrl!,
+        onDone: widget.onDone,
+        muted: mosqueManager.typeIsMosque,
+        onNotFound: () => setState(() => invalidStreamUrl = true),
+      );
     }
-
-    return MawaqitYoutubePlayer(
-      channelId: mosqueManager.mosque!.streamUrl!,
-      onDone: widget.onDone,
-      muted: mosqueManager.typeIsMosque,
-      onNotFound: () => setState(() => invalidStreamUrl = true),
-    );
   }
 }
