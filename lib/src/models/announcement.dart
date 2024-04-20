585e779162b79c4db6832fdd917939206e14239b
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2024-04-21 01:19:47 +0200

    fix: exclude the video from caches

--- lib/src/models/announcement.dart
+++ lib/src/models/announcement.dart
@@ -158,8 +158,5 @@ class Announcement {
 
   /// [isCacheable] returns true if the object can be cached
   /// if the announcement has a video it should not be cached
-  bool isCacheable() {
-    if (video != null && video!.isNotEmpty) return false;
-    return true;
-  }
+  bool get isCacheable => video == null;
 }
