54b1609bae3f4a1bb85901309973f30f5511c07d
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2024-04-20 14:34:31 +0200

    add: add more logs for dispose and initialize for riverpod

--- lib/src/helpers/riverpod_logger.dart
+++ lib/src/helpers/riverpod_logger.dart
@@ -4,6 +4,23 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class RiverpodLogger extends ProviderObserver {
   @override
+  void didAddProvider(
+    ProviderBase<Object?> provider,
+    Object? value,
+    ProviderContainer container,
+  ) {
+    log('Provider $provider was initialized with $value', name: 'RiverpodLogger');
+  }
+
+  @override
+  void didDisposeProvider(
+    ProviderBase<Object?> provider,
+    ProviderContainer container,
+  ) {
+    log('Provider $provider was disposed', name: 'RiverpodLogger');
+  }
+
+  @override
   void didUpdateProvider(
     ProviderBase<Object?> provider,
     Object? previousValue,
@@ -16,6 +33,6 @@ class RiverpodLogger extends ProviderObserver {
           "newValue": "$newValue",
           "previousValue": "$previousValue",
        }
-    ''');
+    ''', name: 'RiverpodLogger');
   }
 }
