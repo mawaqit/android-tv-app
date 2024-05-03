485c1109e27c24bddf4f909ae69246a65f7e9574
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2024-05-04 02:41:44 +0300

    feat: register model in the main

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:logger/logger.dart';
 import 'package:mawaqit/i18n/AppLanguage.dart';
 import 'package:mawaqit/i18n/l10n.dart';
 import 'package:mawaqit/src/const/constants.dart';
+import 'package:mawaqit/src/domain/model/quran/surah_model.dart';
 import 'package:mawaqit/src/enum/connectivity_status.dart';
 import 'package:mawaqit/src/helpers/AnalyticsWrapper.dart';
 import 'package:mawaqit/src/helpers/Api.dart';
@@ -33,6 +34,7 @@ Future<void> main() async {
     () async {
       WidgetsFlutterBinding.ensureInitialized();
       await Firebase.initializeApp();
+      Hive.registerAdapter(SurahModelAdapter());
       await Hive.initFlutter();
       runApp(
         ProviderScope(
