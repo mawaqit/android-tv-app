34b5beda62318ea41969dee3d19e5eff1766fd27
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2024-05-04 02:46:30 +0300

    feat(state_management): add notifier and state for handling the state of the quran feature

---
+++ lib/src/state_management/quran/quran/quran_notifier.dart
@@ -0,0 +1,36 @@
+import 'dart:developer';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mawaqit/src/state_management/quran/quran/quran_state.dart';
+
+import 'package:mawaqit/src/data/repository/quran/quran_impl.dart';
+
+import 'package:mawaqit/src/module/shared_preference_module.dart';
+
+import 'package:mawaqit/src/helpers/language_helper.dart';
+
+class QuranNotifier extends AsyncNotifier<QuranState> {
+  @override
+  build() => QuranState();
+
+  /// [getSuwarByLanguage] get the list of surwars by language
+  ///
+  /// [languageCode] is the language code of the surwars
+  Future<void> getSuwarByLanguage({
+    String languageCode = 'en',
+  }) async {
+    state = AsyncLoading();
+    state = await AsyncValue.guard(() async {
+      final quranRepository = await ref.read(quranRepositoryProvider.future);
+      final sharedPreference = await ref.read(sharedPreferenceModule.future);
+      final languageCode = sharedPreference.getString('language_code') ?? 'en';
+      // return boolean if the languages has locale
+      final mappedLanguage = LanguageHelper.mapLocaleWithQuran(languageCode);
+      log('quran: QuranNotifier: getSuwarByLanguage: languageCode: $languageCode');
+      final suwar = await quranRepository.getSuwarByLanguage(languageCode: mappedLanguage);
+      return state.value!.copyWith(suwar: suwar);
+    });
+  }
+}
+
+final quranNotifierProvider = AsyncNotifierProvider<QuranNotifier, QuranState>(QuranNotifier.new);
