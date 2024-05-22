efb2bb1c4527acdb5d98cab27074ceafeca0e5a3
Ghassen Ben Zahra <ghassen.benzahra@gmail.com>
2024-05-22 11:11:07 +0100

    add missing import

--- lib/src/pages/SettingScreen.dart
+++ lib/src/pages/SettingScreen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart' show AsyncValueX, ConsumerWidget, WidgetRef, Consumer;
 import 'package:mawaqit/i18n/l10n.dart';
