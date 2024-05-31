54d76f3f6561db647cfcce96b3ff9d88bdefbccf
Ghassen Ben Zahra <ghassen.benzahra@gmail.com>
2024-05-31 14:42:57 +0100

    fix focus issue Timezone List

--- lib/src/pages/onBoarding/widgets/onboarding_timezone_selector.dart
+++ lib/src/pages/onBoarding/widgets/onboarding_timezone_selector.dart
@@ -22,16 +22,33 @@ class _OnBoardingTimeZoneSelectorState
   late List<Country> countriesList;
   late List<String> selectedCountryTimezones;
   TextEditingController searchController = TextEditingController();
+  FocusNode searchFocusNode = FocusNode();
+  FocusNode firstCountryListTileFocusNode =
+      FocusNode(); // Add FocusNode for the first ListTile in country list
+  FocusNode firstTimezoneListTileFocusNode =
+      FocusNode(); // Add FocusNode for the first ListTile in timezone list
   int selectedCountryIndex = -1; // Track selected country index
   int selectedTimezoneIndex = -1; // Track selected timezone index
   bool isViewingTimezones =
       false; // Track whether timezone list is being viewed
+  late FocusScopeNode focusScopeNode;
 
   @override
   void initState() {
     super.initState();
     countriesList = Countries.list;
     selectedCountryTimezones = [];
+    focusScopeNode = FocusScopeNode();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    searchFocusNode.dispose();
+    firstCountryListTileFocusNode.dispose();
+    firstTimezoneListTileFocusNode.dispose();
+    focusScopeNode.dispose();
+    super.dispose();
   }
 
   @override
@@ -39,38 +56,48 @@ class _OnBoardingTimeZoneSelectorState
     final themeData = Theme.of(context);
 
     return FocusScope(
-      child: Column(
-        children: [
-          SizedBox(height: 10),
-          Text(
-            S.of(context).appTimezone,
-            style: TextStyle(
-              fontSize: 25.0,
-              fontWeight: FontWeight.w700,
-              color: themeData.brightness == Brightness.dark
-                  ? null
-                  : themeData.primaryColor,
+      node: focusScopeNode,
+      child: WillPopScope(
+        onWillPop: () async {
+          if (searchFocusNode.hasFocus) {
+            searchFocusNode.unfocus();
+            return false;
+          }
+          return true;
+        },
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            Text(
+              S.of(context).appTimezone,
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.w700,
+                color: themeData.brightness == Brightness.dark
+                    ? null
+                    : themeData.primaryColor,
+              ),
             ),
-          ),
-          SizedBox(height: 8),
-          Text(
-            S.of(context).descTimezone,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 15,
-              color: themeData.brightness == Brightness.dark
-                  ? null
-                  : themeData.primaryColor,
+            SizedBox(height: 8),
+            Text(
+              S.of(context).descTimezone,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+                color: themeData.brightness == Brightness.dark
+                    ? null
+                    : themeData.primaryColor,
+              ),
             ),
-          ),
-          SizedBox(height: 20),
-          _buildSearchBar(),
-          Expanded(
-            child: isViewingTimezones
-                ? _buildTimezoneList(context)
-                : _buildCountryList(context),
-          ),
-        ],
+            SizedBox(height: 20),
+            _buildSearchBar(),
+            Expanded(
+              child: isViewingTimezones
+                  ? _buildTimezoneList(context)
+                  : _buildCountryList(context),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -80,6 +107,7 @@ class _OnBoardingTimeZoneSelectorState
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: TextField(
         controller: searchController,
+        focusNode: searchFocusNode,
         onChanged: (value) {
           setState(() {
             countriesList = Countries.list
@@ -87,6 +115,11 @@ class _OnBoardingTimeZoneSelectorState
                     country.name.toLowerCase().contains(value.toLowerCase()))
                 .toList();
           });
+        },
+        onSubmitted: (value) {
+          searchFocusNode.unfocus();
+          FocusScope.of(context).requestFocus(
+              firstCountryListTileFocusNode); // Move focus to the first ListTile
         },
         decoration: InputDecoration(
           hintText: S.of(context).searchCountries,
@@ -112,17 +145,18 @@ class _OnBoardingTimeZoneSelectorState
             Divider(height: 1),
         itemBuilder: (BuildContext context, int index) {
           var country = countriesList[index];
-          return InkWell(
-            onTap: () {
-              setState(() {
-                selectedCountryIndex = index; // Update selected index
-                selectedTimezoneIndex = -1; // Reset timezone index
-                selectedCountryTimezones = country.timezones;
-                isViewingTimezones = true; // Switch to timezone list view
-              });
-            },
+          return Focus(
+            focusNode: index == 0 ? firstCountryListTileFocusNode : FocusNode(),
             child: ListTile(
               title: Text("${country.name}"),
+              onTap: () {
+                setState(() {
+                  selectedCountryIndex = index;
+                  selectedTimezoneIndex = -1;
+                  selectedCountryTimezones = country.timezones;
+                  isViewingTimezones = true;
+                });
+              },
             ),
           );
         },
@@ -133,46 +167,36 @@ class _OnBoardingTimeZoneSelectorState
   Widget _buildTimezoneList(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(top: 5),
-      child: WillPopScope(
-        onWillPop: () async {
-          // Handle back button press
-          if (!isViewingTimezones) {
-            setState(() {
-              isViewingTimezones = false; // Switch back to country list view
-            });
-            return false; // Prevent back navigation
-          }
-          return true; // Allow back navigation if not viewing timezones
-        },
-        child: ListView.separated(
-          padding: EdgeInsets.only(
-            top: 5,
-            bottom: 5,
-          ),
-          itemCount: selectedCountryTimezones.length,
-          separatorBuilder: (BuildContext context, int index) =>
-              Divider(height: 1),
-          itemBuilder: (BuildContext context, int index) {
-            var timezone = selectedCountryTimezones[index];
-            var detroit = tz.getLocation(timezone);
-            var now = tz.TZDateTime.now(detroit);
-            var timeZoneOffset = now.timeZoneOffset;
-            return InkWell(
+      child: ListView.separated(
+        padding: EdgeInsets.only(
+          top: 5,
+          bottom: 5,
+        ),
+        itemCount: selectedCountryTimezones.length,
+        separatorBuilder: (BuildContext context, int index) =>
+            Divider(height: 1),
+        itemBuilder: (BuildContext context, int index) {
+          var timezone = selectedCountryTimezones[index];
+          var detroit = tz.getLocation(timezone);
+          var now = tz.TZDateTime.now(detroit);
+          var timeZoneOffset = now.timeZoneOffset;
+          return Focus(
+            focusNode:
+                index == 0 ? firstTimezoneListTileFocusNode : FocusNode(),
+            child: ListTile(
+              title: Text(
+                convertToGMTOffset(timeZoneOffset) + " $timezone",
+              ),
               onTap: () async {
                 setState(() {
-                  selectedTimezoneIndex = index; // Update selected index
+                  selectedTimezoneIndex = index;
                 });
                 await _setDeviceTimezone(timezone);
                 widget.onSelect?.call();
               },
-              child: ListTile(
-                title: Text(
-                  convertToGMTOffset(timeZoneOffset) + " $timezone",
-                ),
-              ),
-            );
-          },
-        ),
+            ),
+          );
+        },
       ),
     );
   }
