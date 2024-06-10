d72ee9c1c0e70a2f9b41c17a7d911a805cd2f3da
Ghassen Ben Zahra <ghassen.benzahra@gmail.com>
2024-06-10 19:30:54 +0100

    code format

--- lib/src/pages/SettingScreen.dart
+++ lib/src/pages/SettingScreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart'
-    show AsyncValueX, ConsumerWidget, WidgetRef, Consumer;
+import 'package:flutter_riverpod/flutter_riverpod.dart' show AsyncValueX, ConsumerWidget, WidgetRef, Consumer;
 import 'package:mawaqit/i18n/l10n.dart';
 import 'package:mawaqit/src/helpers/AppRouter.dart';
 import 'package:mawaqit/src/helpers/connectivity_provider.dart';
@@ -55,8 +54,7 @@ class SettingScreen extends ConsumerWidget {
     final userPreferences = context.watch<UserPreferencesManager>();
     final themeManager = context.watch<ThemeNotifier>();
     final String checkInternet = S.of(context).noInternet;
-    final String checkInternetLegacyMode =
-        S.of(context).checkInternetLegacyMode;
+    final String checkInternetLegacyMode = S.of(context).checkInternetLegacyMode;
     final String hadithLanguage = S.of(context).connectToChangeHadith;
     TimeShiftManager timeShiftManager = TimeShiftManager();
     final featureManager = Provider.of<FeatureManager>(context);
@@ -105,15 +103,12 @@ class SettingScreen extends ConsumerWidget {
                           isIconActivated: true,
                           title: S.of(context).randomHadithLanguage,
                           description: S.of(context).descLang,
-                          languages:
-                              appLanguage.hadithLocalizedLanguage.keys.toList(),
+                          languages: appLanguage.hadithLocalizedLanguage.keys.toList(),
                           isSelected: (langCode) {
                             return appLanguage.hadithLanguage == langCode;
                           },
                           onSelect: (langCode) async {
-                            await ref
-                                .read(connectivityProvider.notifier)
-                                .checkInternetConnection();
+                            await ref.read(connectivityProvider.notifier).checkInternetConnection();
                             ref.watch(connectivityProvider).maybeWhen(
                               orElse: () {
                                 showCheckInternetDialog(
@@ -126,8 +121,7 @@ class SettingScreen extends ConsumerWidget {
                                 );
                               },
                               data: (isConnectedToInternet) {
-                                if (isConnectedToInternet ==
-                                    ConnectivityStatus.disconnected) {
+                                if (isConnectedToInternet == ConnectivityStatus.disconnected) {
                                   showCheckInternetDialog(
                                     context: context,
                                     onRetry: () {
@@ -137,12 +131,9 @@ class SettingScreen extends ConsumerWidget {
                                     content: hadithLanguage,
                                   );
                                 } else {
-                                  context
-                                      .read<AppLanguage>()
-                                      .setHadithLanguage(langCode);
+                                  context.read<AppLanguage>().setHadithLanguage(langCode);
                                   ref
-                                      .read(
-                                          randomHadithNotifierProvider.notifier)
+                                      .read(randomHadithNotifierProvider.notifier)
                                       .fetchAndCacheHadith(language: langCode);
                                   AppRouter.pop();
                                 }
@@ -161,9 +152,7 @@ class SettingScreen extends ConsumerWidget {
                         icon: Icon(Icons.update, size: 35),
                         onChanged: (value) {
                           logger.d('setting: disable the update $value');
-                          ref
-                              .read(appUpdateProvider.notifier)
-                              .toggleAutoUpdateChecking();
+                          ref.read(appUpdateProvider.notifier).toggleAutoUpdateChecking();
                         },
                         value: ref.watch(appUpdateProvider).maybeWhen(
                               orElse: () => false,
@@ -181,9 +170,7 @@ class SettingScreen extends ConsumerWidget {
                     textAlign: TextAlign.center,
                   ),
                   _SettingSwitchItem(
-                    title: theme.brightness == Brightness.light
-                        ? S.of(context).darkMode
-                        : S.of(context).lightMode,
+                    title: theme.brightness == Brightness.light ? S.of(context).darkMode : S.of(context).lightMode,
                     icon: Icon(Icons.brightness_4, size: 35),
                     onChanged: (value) => themeManager.toggleMode(),
                     value: themeManager.isLightTheme ?? false,
@@ -209,8 +196,7 @@ class SettingScreen extends ConsumerWidget {
                           onTap: () {
                             showDialog(
                               context: context,
-                              builder: (context) => TimePickerModal(
-                                  timeShiftManager: timeShiftManager),
+                              builder: (context) => TimePickerModal(timeShiftManager: timeShiftManager),
                             );
                           },
                         )
@@ -221,30 +207,23 @@ class SettingScreen extends ConsumerWidget {
                       subtitle: S.of(context).announcementOnlyModeEXPLINATION,
                       icon: Icon(Icons.notifications, size: 35),
                       value: userPreferences.announcementsOnly,
-                      onChanged: (value) =>
-                          userPreferences.announcementsOnly = value,
+                      onChanged: (value) => userPreferences.announcementsOnly = value,
                     ),
-                  if (!userPreferences.webViewMode &&
-                      !userPreferences.announcementsOnly)
+                  if (!userPreferences.webViewMode && !userPreferences.announcementsOnly)
                     _SettingSwitchItem(
                       title: S.of(context).secondaryScreen,
                       subtitle: S.of(context).secondaryScreenExplanation,
                       value: userPreferences.isSecondaryScreen,
                       icon: Icon(Icons.monitor, size: 35),
-                      onChanged: (value) =>
-                          userPreferences.isSecondaryScreen = value,
+                      onChanged: (value) => userPreferences.isSecondaryScreen = value,
                     ),
                   _SettingSwitchItem(
                     title: S.of(context).webView,
-                    subtitle: S
-                        .of(context)
-                        .ifYouAreFacingAnIssueWithTheAppActivateThis,
+                    subtitle: S.of(context).ifYouAreFacingAnIssueWithTheAppActivateThis,
                     icon: Icon(Icons.online_prediction, size: 35),
                     value: userPreferences.webViewMode,
                     onChanged: (value) async {
-                      await ref
-                          .read(connectivityProvider.notifier)
-                          .checkInternetConnection();
+                      await ref.read(connectivityProvider.notifier).checkInternetConnection();
                       ref.watch(connectivityProvider).maybeWhen(
                         orElse: () {
                           showCheckInternetDialog(
@@ -257,8 +236,7 @@ class SettingScreen extends ConsumerWidget {
                           );
                         },
                         data: (isConnectedToInternet) {
-                          if (isConnectedToInternet ==
-                              ConnectivityStatus.disconnected) {
+                          if (isConnectedToInternet == ConnectivityStatus.disconnected) {
                             showCheckInternetDialog(
                               context: context,
                               onRetry: () {
@@ -310,10 +288,7 @@ class _SettingItem extends StatelessWidget {
         trailing: Icon(Icons.arrow_forward_ios),
         title: Text(title),
         subtitle: subtitle != null
-            ? Text(subtitle!,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(fontSize: 10))
+            ? Text(subtitle!, maxLines: 2, overflow: TextOverflow.ellipsis, style: TextStyle(fontSize: 10))
             : null,
         onTap: onTap,
       ),
@@ -347,9 +322,7 @@ class _SettingSwitchItem extends StatelessWidget {
         autofocus: true,
         secondary: icon ?? SizedBox(),
         title: Text(title),
-        subtitle: subtitle != null
-            ? Text(subtitle!, maxLines: 2, overflow: TextOverflow.clip)
-            : null,
+        subtitle: subtitle != null ? Text(subtitle!, maxLines: 2, overflow: TextOverflow.clip) : null,
         value: value,
         onChanged: onChanged,
       ),
--- lib/src/widgets/MawaqitWebViewWidget.dart
+++ lib/src/widgets/MawaqitWebViewWidget.dart
@@ -31,8 +31,7 @@ class MawaqitWebViewWidget extends StatefulWidget {
   /// clean up web specific component like header - footer - breadcrumb
   final bool clean;
 
-  MawaqitWebViewWidget({Key? key, this.path, this.clean = false})
-      : super(key: key);
+  MawaqitWebViewWidget({Key? key, this.path, this.clean = false}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
@@ -55,8 +54,7 @@ class MawaqitWebViewWidgetState extends State<MawaqitWebViewWidget>
   bool hasError = false;
 
   final Set<Factory<OneSequenceGestureRecognizer>> _gSet = [
-    Factory<VerticalDragGestureRecognizer>(
-        () => VerticalDragGestureRecognizer()),
+    Factory<VerticalDragGestureRecognizer>(() => VerticalDragGestureRecognizer()),
     Factory<ScaleGestureRecognizer>(() => ScaleGestureRecognizer()),
     Factory<PanGestureRecognizer>(() => PanGestureRecognizer()),
   ].toSet();
@@ -74,8 +72,7 @@ class MawaqitWebViewWidgetState extends State<MawaqitWebViewWidget>
         if (Platform.isAndroid) {
           webViewController?.reload();
         } else if (Platform.isIOS) {
-          webViewController?.loadUrl(
-              urlRequest: URLRequest(url: await webViewController?.getUrl()));
+          webViewController?.loadUrl(urlRequest: URLRequest(url: await webViewController?.getUrl()));
         }
       },
     );
@@ -113,9 +110,8 @@ class MawaqitWebViewWidgetState extends State<MawaqitWebViewWidget>
 
   @override
   void dispose() {
-    webViewGPSPositionStreams.forEach(
-        (StreamSubscription<Position> _flutterGeolocationStream) =>
-            _flutterGeolocationStream.cancel());
+    webViewGPSPositionStreams
+        .forEach((StreamSubscription<Position> _flutterGeolocationStream) => _flutterGeolocationStream.cancel());
     super.dispose();
   }
 
@@ -155,9 +151,7 @@ class MawaqitWebViewWidgetState extends State<MawaqitWebViewWidget>
                     useShouldOverrideUrlLoading: true,
                     useOnDownloadStart: true,
                     mediaPlaybackRequiresUserGesture: false,
-                    userAgent: Platform.isAndroid
-                        ? settings.userAgent!.valueAndroid!
-                        : settings.userAgent!.valueIOS!,
+                    userAgent: Platform.isAndroid ? settings.userAgent!.valueAndroid! : settings.userAgent!.valueIOS!,
                   ),
                   android: AndroidInAppWebViewOptions(
                     useHybridComposition: true,
@@ -165,8 +159,7 @@ class MawaqitWebViewWidgetState extends State<MawaqitWebViewWidget>
                   ios: IOSInAppWebViewOptions(
                     allowsInlineMediaPlayback: true,
                   )),
-              pullToRefreshController:
-                  settings.pullRefresh == "1" ? pullToRefreshController : null,
+              pullToRefreshController: settings.pullRefresh == "1" ? pullToRefreshController : null,
               onWebViewCreated: (InAppWebViewController controller) {
                 webViewController = controller;
                 controller.addJavaScriptHandler(
@@ -186,35 +179,25 @@ class MawaqitWebViewWidgetState extends State<MawaqitWebViewWidget>
 
                       switch (action) {
                         case "clearWatch":
-                          _geolocationClearWatch(parseInt(
-                              geolocationData['flutterGeolocationIndex'] ??
-                                  0)!);
+                          _geolocationClearWatch(parseInt(geolocationData['flutterGeolocationIndex'] ?? 0)!);
                           break;
 
                         case "getCurrentPosition":
-                          _geolocationGetCurrentPosition(
-                              parseInt(
-                                  geolocationData['flutterGeolocationIndex'] ??
-                                      0),
-                              PositionOptions()
-                                  .from(geolocationData['option'] ?? null));
+                          _geolocationGetCurrentPosition(parseInt(geolocationData['flutterGeolocationIndex'] ?? 0),
+                              PositionOptions().from(geolocationData['option'] ?? null));
                           break;
 
                         case "watchPosition":
-                          _geolocationWatchPosition(
-                              parseInt(
-                                  geolocationData['flutterGeolocationIndex'] ??
-                                      0)!,
-                              PositionOptions()
-                                  .from(geolocationData['option'] ?? null));
+                          _geolocationWatchPosition(parseInt(geolocationData['flutterGeolocationIndex'] ?? 0)!,
+                              PositionOptions().from(geolocationData['option'] ?? null));
                           break;
                         default:
                       }
                     });
               },
-              androidOnPermissionRequest: (InAppWebViewController controller,
-                      String origin, List<String> resources) async =>
-                  PermissionRequestResponse(
+              androidOnPermissionRequest:
+                  (InAppWebViewController controller, String origin, List<String> resources) async =>
+                      PermissionRequestResponse(
                 resources: resources,
                 action: PermissionRequestResponseAction.GRANT,
               ),
@@ -224,29 +207,18 @@ class MawaqitWebViewWidgetState extends State<MawaqitWebViewWidget>
                 print(uri.scheme);
                 if (Platform.isAndroid && ["intent"].contains(uri.scheme)) {
                   if (uri.toString().indexOf("maps") != -1) {
-                    var link = uri
-                        .toString()
-                        .substring(uri.toString().indexOf('?link=') + 6);
+                    var link = uri.toString().substring(uri.toString().indexOf('?link=') + 6);
                     // print(link);
-                    AndroidIntent intent =
-                        AndroidIntent(action: 'action_view', data: link);
+                    AndroidIntent intent = AndroidIntent(action: 'action_view', data: link);
                     await intent.launch();
                   } else {
-                    String id = uri.toString().substring(
-                        uri.toString().indexOf('id%3D') + 5,
-                        uri.toString().indexOf('#Intent'));
+                    String id = uri
+                        .toString()
+                        .substring(uri.toString().indexOf('id%3D') + 5, uri.toString().indexOf('#Intent'));
                     await StoreRedirect.redirect(androidAppId: id);
                   }
                   return NavigationActionPolicy.CANCEL;
-                } else if (![
-                  "http",
-                  "https",
-                  "chrome",
-                  "data",
-                  "javascript",
-                  "file",
-                  "about"
-                ].contains(uri.scheme)) {
+                } else if (!["http", "https", "chrome", "data", "javascript", "file", "about"].contains(uri.scheme)) {
                   if (await canLaunchUrl(uri)) {
                     // Launch the App
                     await launchUrl(uri);
@@ -261,8 +233,7 @@ class MawaqitWebViewWidgetState extends State<MawaqitWebViewWidget>
               },
               onLoadStop: (controller, url) async {
                 pullToRefreshController!.endRefreshing();
-                Future.delayed(const Duration(milliseconds: 500),
-                    () => _geolocationAlertFix());
+                Future.delayed(const Duration(milliseconds: 500), () => _geolocationAlertFix());
                 if (widget.clean) {
                   await webViewController!.injectJavascriptFileFromAsset(
                     assetFilePath: R.ASSETS_SCRIPTS_CLEAN_JS,
@@ -344,24 +315,19 @@ class MawaqitWebViewWidgetState extends State<MawaqitWebViewWidget>
     return int.tryParse(value) ?? null;
   }
 
-  Future<PositionResponse> getCurrentPosition(
-      PositionOptions positionOptions) async {
+  Future<PositionResponse> getCurrentPosition(PositionOptions positionOptions) async {
     PositionResponse positionResponse = PositionResponse();
 
     int? timeout = 30000;
     if (positionOptions.timeout! > 0) timeout = positionOptions.timeout;
 
     try {
-      LocationPermission geolocationStatus =
-          await GeolocatorPlatform.instance.requestPermission();
+      LocationPermission geolocationStatus = await GeolocatorPlatform.instance.requestPermission();
 
-      if (geolocationStatus == LocationPermission.always ||
-          geolocationStatus == LocationPermission.whileInUse) {
+      if (geolocationStatus == LocationPermission.always || geolocationStatus == LocationPermission.whileInUse) {
         positionResponse.position = await Future.any([
           Geolocator.getCurrentPosition(
-              desiredAccuracy: (positionOptions.enableHighAccuracy
-                  ? LocationAccuracy.best
-                  : LocationAccuracy.medium)),
+              desiredAccuracy: (positionOptions.enableHighAccuracy ? LocationAccuracy.best : LocationAccuracy.medium)),
           Future.delayed(Duration(milliseconds: timeout!), () {
             if (positionOptions.timeout! > 0) positionResponse.timedOut = true;
             return;
@@ -480,20 +446,14 @@ class MawaqitWebViewWidgetState extends State<MawaqitWebViewWidget>
     webViewController!.evaluateJavascript(source: javascript);
   }
 
-  void _geolocationGetCurrentPosition(
-      int? flutterGeolocationIndex, PositionOptions positionOptions) async {
-    PositionResponse positionResponse =
-        await getCurrentPosition(positionOptions);
+  void _geolocationGetCurrentPosition(int? flutterGeolocationIndex, PositionOptions positionOptions) async {
+    PositionResponse positionResponse = await getCurrentPosition(positionOptions);
 
-    _geolocationResponse(
-        flutterGeolocationIndex, positionOptions, positionResponse, false);
+    _geolocationResponse(flutterGeolocationIndex, positionOptions, positionResponse, false);
   }
 
   void _geolocationResponse(
-      int? flutterGeolocationIndex,
-      PositionOptions positionOptions,
-      PositionResponse positionResponse,
-      bool watcher) {
+      int? flutterGeolocationIndex, PositionOptions positionOptions, PositionResponse positionResponse, bool watcher) {
     if (positionResponse.position != null) {
       String javascript = '''
         function _flutterGeolocationResponse() {
@@ -520,20 +480,11 @@ class MawaqitWebViewWidgetState extends State<MawaqitWebViewWidget>
           '''
             },
             timestamp: ''' +
-          positionResponse.position!.timestamp!.millisecondsSinceEpoch
-              .toString() +
+          positionResponse.position!.timestamp!.millisecondsSinceEpoch.toString() +
           '''
           });''' +
-          (!watcher
-              ? "  _flutterGeolocationSuccess[" +
-                  flutterGeolocationIndex.toString() +
-                  "] = null; "
-              : "") +
-          (!watcher
-              ? "  _flutterGeolocationError[" +
-                  flutterGeolocationIndex.toString() +
-                  "] = null; "
-              : "") +
+          (!watcher ? "  _flutterGeolocationSuccess[" + flutterGeolocationIndex.toString() + "] = null; " : "") +
+          (!watcher ? "  _flutterGeolocationError[" + flutterGeolocationIndex.toString() + "] = null; " : "") +
           '''
           return true;
         };
@@ -556,16 +507,8 @@ class MawaqitWebViewWidgetState extends State<MawaqitWebViewWidget>
                   flutterGeolocationIndex.toString() +
                   "]({code: 1, message: 'User denied Geolocationg', PERMISSION_DENIED: 1, POSITION_UNAVAILABLE: 2, TIMEOUT: 3}); ") +
           "}" +
-          (!watcher
-              ? "  _flutterGeolocationSuccess[" +
-                  flutterGeolocationIndex.toString() +
-                  "] = null; "
-              : "") +
-          (!watcher
-              ? "  _flutterGeolocationError[" +
-                  flutterGeolocationIndex.toString() +
-                  "] = null; "
-              : "") +
+          (!watcher ? "  _flutterGeolocationSuccess[" + flutterGeolocationIndex.toString() + "] = null; " : "") +
+          (!watcher ? "  _flutterGeolocationError[" + flutterGeolocationIndex.toString() + "] = null; " : "") +
           '''
           return true;
         };
@@ -582,19 +525,14 @@ class MawaqitWebViewWidgetState extends State<MawaqitWebViewWidget>
   ) {
     // init new strem
     var locationOptions = LocationSettings(
-        accuracy: (positionOptions.enableHighAccuracy
-            ? LocationAccuracy.best
-            : LocationAccuracy.medium),
+        accuracy: (positionOptions.enableHighAccuracy ? LocationAccuracy.best : LocationAccuracy.medium),
         distanceFilter: 10);
 
     webViewGPSPositionStreams[flutterGeolocationIndex] =
-        Geolocator.getPositionStream(locationSettings: locationOptions)
-            .listen((Position position) {
+        Geolocator.getPositionStream(locationSettings: locationOptions).listen((Position position) {
       // Send data to each warcher
-      PositionResponse positionResponse = PositionResponse()
-        ..position = position;
-      _geolocationResponse(
-          flutterGeolocationIndex, positionOptions, positionResponse, true);
+      PositionResponse positionResponse = PositionResponse()..position = position;
+      _geolocationResponse(flutterGeolocationIndex, positionOptions, positionResponse, true);
     });
   }
 
