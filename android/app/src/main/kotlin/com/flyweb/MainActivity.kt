24fe47937eb3dbf5b687e7d9c0972e9aacc943a9
Ghassen Ben Zahra <ghassen.benzahra@gmail.com>
2024-06-16 17:39:20 +0100

    fix return type checkroot

--- android/app/src/main/kotlin/com/flyweb/MainActivity.kt
+++ android/app/src/main/kotlin/com/flyweb/MainActivity.kt
@@ -22,8 +22,8 @@ import android.os.Build.VERSION_CODES
 import android.app.admin.DevicePolicyManager
 import java.io.IOException
 class MainActivity : FlutterActivity() {
-private lateinit var mAdminComponentName: ComponentName
-  private lateinit var mDevicePolicyManager: DevicePolicyManager
+    private lateinit var mAdminComponentName: ComponentName
+    private lateinit var mDevicePolicyManager: DevicePolicyManager
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
@@ -34,78 +34,70 @@ private lateinit var mAdminComponentName: ComponentName
                     "setDeviceTimezone" -> setDeviceTimezone(call, result)
                     "connectToWifi" -> connectToWifi(call, result)
                     "isPackageInstalled" -> {
-                            val packageName = call.argument<String>("packageName")
-                            if (packageName != null) {
-                                val isInstalled = isPackageInstalled(packageName)
-                                result.success(isInstalled)
-                            } else {
-                                result.error("INVALID_ARGUMENT", "Package name is null", null)
-                            }
-                        }  
-                        "checkRoot"->checkRoot(result)
-                        "clearAppData"->{
-                            val isSuccess = clearDataRestart()
-        result.success(isSuccess)
+                        val packageName = call.argument<String>("packageName")
+                        if (packageName != null) {
+                            val isInstalled = isPackageInstalled(packageName)
+                            result.success(isInstalled)
+                        } else {
+                            result.error("INVALID_ARGUMENT", "Package name is null", null)
                         }
-           
-                             else -> result.notImplemented()
+                    }
+                    "checkRoot" -> result.success(checkRoot())
+                    "toggleScreenOn" -> toggleScreenOn(call, result)
+                    "toggleScreenOff" -> toggleScreenOff(call, result)
+                    "toggleBoxScreenOff" -> toggleBoxScreenOff(call, result)
+                    "toggleBoxScreenOn" -> toggleBoxScreenOn(call, result)
+                    "clearAppData" -> {
+                        val isSuccess = clearDataRestart()
+                        result.success(isSuccess)
+                    }
+                    else -> result.notImplemented()
                 }
             }
     }
-private fun checkRoot(result: MethodChannel.Result) {
-    try {
-        val p = Runtime.getRuntime().exec("su")
-        val os = DataOutputStream(p.outputStream)
-        os.writeBytes("echo \"Do I have root?\" >/data/LandeRootCheck.txt\n")
-        os.writeBytes("exit\n")
-        os.flush()
-        try {
+
+    private fun checkRoot(): Boolean {
+        return try {
+            val p = Runtime.getRuntime().exec("su")
+            val os = DataOutputStream(p.outputStream)
+            os.writeBytes("echo \"Do I have root?\" >/data/LandeRootCheck.txt\n")
+            os.writeBytes("exit\n")
+            os.flush()
             p.waitFor()
-            if (p.exitValue() == 0) {
-                result.success(true)
-            } else {
-                result.success(false)
-            }
-        } catch (e: InterruptedException) {
-            result.error("InterruptedException", "Interrupted exception occurred", null)
+            p.exitValue() == 0
+        } catch (e: Exception) {
+            false
         }
-    } catch (e: IOException) {
-        result.error("IOException", "I/O exception occurred", null)
     }
-}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var REQUEST_OVERLAY_PERMISSIONS = 100
-        if (checkRoot() && !Settings.canDrawOverlays(getApplicationContext())) {
-             try {
-        val command = "appops set com.mawaqit.androidtv SYSTEM_ALERT_WINDOW allow"
-        val process = Runtime.getRuntime().exec(arrayOf("su", "-c", command))
-        val outputStream = DataOutputStream(process.outputStream)
-        outputStream.writeBytes(command + "\n")
-        outputStream.flush()
-        outputStream.close()
-        process.waitFor()
-        return
-    } catch (e: Exception) {
-        e.printStackTrace()
-    }
-            
+        val REQUEST_OVERLAY_PERMISSIONS = 100
+        if (checkRoot() && !Settings.canDrawOverlays(applicationContext)) {
+            try {
+                val command = "appops set com.mawaqit.androidtv SYSTEM_ALERT_WINDOW allow"
+                val process = Runtime.getRuntime().exec(arrayOf("su", "-c", command))
+                val outputStream = DataOutputStream(process.outputStream)
+                outputStream.writeBytes(command + "\n")
+                outputStream.flush()
+                outputStream.close()
+                process.waitFor()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 
-
-  
- private fun setDeviceTimezone(call: MethodCall, result: MethodChannel.Result) {
-    AsyncTask.execute {
-        try {
-            val timezone = call.argument<String>("timezone")
-            executeCommand("service call alarm 3 s16 $timezone", result)
-        } catch (e: Exception) {
-            handleCommandException(e, result)
+    private fun setDeviceTimezone(call: MethodCall, result: MethodChannel.Result) {
+        AsyncTask.execute {
+            try {
+                val timezone = call.argument<String>("timezone")
+                executeCommand("service call alarm 3 s16 $timezone", result)
+            } catch (e: Exception) {
+                handleCommandException(e, result)
+            }
         }
     }
-}
-
 
     private fun isPackageInstalled(packageName: String?): Boolean {
         val packageManager = applicationContext.packageManager
@@ -116,22 +108,24 @@ private fun checkRoot(result: MethodChannel.Result) {
             false
         }
     }
-private fun connectToWifi(call: MethodCall, result: MethodChannel.Result) {
-    AsyncTask.execute {
-        try {
-            val ssid = call.argument<String>("ssid")
-            val password = call.argument<String>("password")
-            val security = if (password.isNullOrEmpty()) "open" else "wpa2"
 
-            Log.i("SU_COMMAND", "Wifi Command output: cmd wifi connect-network $ssid $security $password")
+    private fun connectToWifi(call: MethodCall, result: MethodChannel.Result) {
+        AsyncTask.execute {
+            try {
+                val ssid = call.argument<String>("ssid")
+                val password = call.argument<String>("password")
+                val security = if (password.isNullOrEmpty()) "open" else "wpa2"
 
-            executeCommand("cmd wifi connect-network $ssid $security $password", result)
+                Log.i("SU_COMMAND", "Wifi Command output: cmd wifi connect-network $ssid $security $password")
 
-        } catch (e: Exception) {
-            handleCommandException(e, result)
+                executeCommand("cmd wifi connect-network $ssid $security $password", result)
+
+            } catch (e: Exception) {
+                handleCommandException(e, result)
+            }
         }
     }
-}
+
     private fun executeCommand(command: String, result: MethodChannel.Result) {
         try {
             val suProcess = Runtime.getRuntime().exec("su")
@@ -151,7 +145,7 @@ private fun connectToWifi(call: MethodCall, result: MethodChannel.Result) {
 
             if (exitCode != 0) {
                 Log.e("SU_COMMAND", "Command failed with exit code $exitCode.")
-                                result.success(false)
+                result.success(false)
 
             } else {
                 Log.i("SU_COMMAND", "Command executed successfully.")
@@ -166,24 +160,113 @@ private fun connectToWifi(call: MethodCall, result: MethodChannel.Result) {
         Log.e("SU_COMMAND", "Exception while executing command: ${e.message}")
         result.error("CMD_ERROR", "Exception while executing command", null)
     }
-      private fun clearDataRestart(): Boolean {
-    try {
-      val processBuilder = ProcessBuilder()
-      processBuilder.command("sh", "-c", """
-            pm clear com.mawaqit.androidtv
-        """.trimIndent())
-      val process = processBuilder.start()
-      val exitCode = process.waitFor()
-      if (exitCode == 0) {
-        return true
-      }
-      return false
-    } catch (e: IOException) {
-      e.printStackTrace()
-      return false
-    } catch (e: InterruptedException) {
-      e.printStackTrace()
-      return false
+
+    private fun clearDataRestart(): Boolean {
+        return try {
+            val processBuilder = ProcessBuilder()
+            processBuilder.command("sh", "-c", """
+                pm clear com.mawaqit.androidtv
+            """.trimIndent())
+            val process = processBuilder.start()
+            val exitCode = process.waitFor()
+            exitCode == 0
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
     }
-  }
+
+    private fun toggleScreenOn(call: MethodCall, result: MethodChannel.Result) {
+        AsyncTask.execute {
+            try {
+                val commands = listOf(
+                    "input keyevent 82",
+                    "am start -W -n com.mawaqit.androidtv/.MainActivity"
+                )
+                executeCommands(commands, result) // Lock the device
+            } catch (e: Exception) {
+                handleCommandExceptions(e, result)
+            }
+        }
+    }
+
+    private fun toggleBoxScreenOff(call: MethodCall, result: MethodChannel.Result) {
+        AsyncTask.execute {
+            try {
+                val commands = listOf(
+                    "mount -o rw,remount /",
+                    "cd /sys/class/hdmi/hdmi/attr",
+                    "echo 0 > phy_power"
+                )
+                executeCommands(commands, result) // Lock the device
+            } catch (e: Exception) {
+                handleCommandExceptions(e, result)
+            }
+        }
+    }
+
+    private fun toggleBoxScreenOn(call: MethodCall, result: MethodChannel.Result) {
+        AsyncTask.execute {
+            try {
+                val commands = listOf(
+                    "mount -o rw,remount /",
+                    "cd /sys/class/hdmi/hdmi/attr",
+                    "echo 1 > phy_power",
+                    "am start -W -n com.mawaqit.androidtv/.MainActivity"
+                )
+                executeCommands(commands, result)
+            } catch (e: Exception) {
+                handleCommandExceptions(e, result)
+            }
+        }
+    }
+
+    private fun toggleScreenOff(call: MethodCall, result: MethodChannel.Result) {
+        AsyncTask.execute {
+            try {
+                executeCommands(listOf("input keyevent 26"), result) // Lock the device
+            } catch (e: Exception) {
+                handleCommandExceptions(e, result)
+            }
+        }
+    }
+
+    private fun executeCommands(commands: List<String>, result: MethodChannel.Result) {
+        try {
+            Log.d("SU_COMMAND", "Executing commands: ${commands.joinToString(separator = " && ")}")
+
+            val suProcess = Runtime.getRuntime().exec("su")
+            val os = DataOutputStream(suProcess.outputStream)
+
+            val command = commands.joinToString(separator = " && ") + "\n"
+            Log.d("SU_COMMAND", "Writing command to DataOutputStream: $command")
+            os.writeBytes(command)
+            os.flush()
+            os.close()
+
+            val output = BufferedReader(InputStreamReader(suProcess.inputStream)).readText()
+            val error = BufferedReader(InputStreamReader(suProcess.errorStream)).readText()
+
+            Log.i("SU_COMMAND", "Command output: $output")
+            Log.e("SU_COMMAND", "Command error: $error")
+
+            val exitCode = suProcess.waitFor()
+            Log.d("SU_COMMAND", "Exit code: $exitCode")
+
+            if (exitCode != 0) {
+                Log.e("SU_COMMAND", "Command failed with exit code $exitCode.")
+                result.error("CMD_ERROR", "Command failed", null)
+            } else {
+                Log.i("SU_COMMAND", "Command executed successfully.")
+                result.success("Command executed successfully.")
+            }
+        } catch (e: Exception) {
+            Log.e("SU_COMMAND", "Exception occurred: ${e.message}")
+            handleCommandExceptions(e, result)
+        }
+    }
+
+    private fun handleCommandExceptions(e: Exception, result: MethodChannel.Result) {
+        result.error("Exception", "An exception occurred: $e", null)
+    }
 }
