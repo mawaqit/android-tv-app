562decdf96adb9a888dc5303f2bdad985b62b895
Ghassen Ben Zahra <ghassen.benzahra@gmail.com>
2024-08-15 14:58:21 +0100

    serialize object to succesfuly store

--- lib/src/helpers/ApiInterceptor.dart
+++ lib/src/helpers/ApiInterceptor.dart
@@ -1,14 +1,17 @@
 import 'package:dio/dio.dart';
 import 'package:hive/hive.dart';
 import 'package:mawaqit/main.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:convert';
 
 class ApiCacheInterceptor extends Interceptor {
   static Box<dynamic>? _box;
 
   static Future<void> init() async {
     if (_box != null) return;
+    final directory = await getApplicationDocumentsDirectory();
 
-    _box = await Hive.openBox('apiCache');
+    _box = await Hive.openBox('apiCache', path: directory.path);
   }
 
   Box<dynamic> get box => _box!;
@@ -21,10 +24,13 @@ class ApiCacheInterceptor extends Interceptor {
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
     final cacheKey = getCacheKey(options);
     try {
-      final cachedData = box.get(cacheKey);
+      logger.i('interceptor: onRequest: Box is open: ${box.isOpen}');
+      logger.i('interceptor: onRequest: Box length: ${box.length}');
+      logger.i('interceptor: onRequest: All keys in box: ${box.keys.toList()}');
+      final cachedData = await box.get(cacheKey);
       logger.i('interceptor: onRequest: Cache key: $cacheKey - cachedData: $cachedData');
+
       if (cachedData != null && options.extra['disableCache'] != true) {
-        logger.i('interceptor: onRequest: Cache key: disableCache $cacheKey - cachedData: $cachedData');
         final lastModified = cachedData['lastModified'];
         if (lastModified != null) {
           options.headers['If-Modified-Since'] = lastModified;
@@ -41,21 +47,21 @@ class ApiCacheInterceptor extends Interceptor {
     try {
       final cacheKey = getCacheKey(response.requestOptions);
       if (response.statusCode == 200) {
-        cacheResponse(cacheKey, response);
+        await cacheResponse(cacheKey, response);
+        logger.i('interceptor: onResponse: Cache key: $cacheKey - cachedData: ${response.data}');
       }
-      logger.i('interceptor: onResponse: Cache key: $cacheKey - cachedData: $response');
     } catch (e) {
       logger.e('interceptor: Error caching response: $e');
     }
     handler.next(response);
   }
 
-  void cacheResponse(String cacheKey, Response response) {
+  Future<void> cacheResponse(String cacheKey, Response response) async {
     final cacheData = {
-      'data': response,
-      'lastModified': response.headers["Last-Modified"],
+      'data': json.encode(response.data),
+      'lastModified': response.headers.value("Last-Modified"),
     };
-    box.put(cacheKey, cacheData);
+    await box.put(cacheKey, cacheData);
   }
 
   @override
@@ -63,11 +69,12 @@ class ApiCacheInterceptor extends Interceptor {
     if (err.response?.statusCode == 404) return handler.next(err);
 
     final cacheKey = getCacheKey(err.requestOptions);
-    final cachedData = box.get(cacheKey);
+    final cachedData = await box.get(cacheKey);
 
     if (cachedData != null && err.requestOptions.extra['disableCache'] != true) {
+      final responseData = json.decode(cachedData['data']);
       final response = Response(
-        data: cachedData['data'],
+        data: responseData,
         headers: Headers.fromMap({'last-modified': cachedData['lastModified']}),
         statusCode: 200,
         requestOptions: err.requestOptions,
