47b3e11d6623549d61d33025344022d9e661535d
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2024-08-20 21:36:40 +0300

    fix: fgen for automatic asset generation and fix asset references (#1280)
    * faet/fix: fgen for automatic asset generation and fix asset references
    
    - Add fgen.yaml configuration file
    - Update lib/const/resource.dart with new asset references
    - Fix incorrect asset path in multiple files
    - Update asset generation comments
    
    * fix: reformatting

--- lib/const/resource.dart
+++ lib/const/resource.dart
@@ -1,160 +1,222 @@
-/// Generate by [resource_generator](https://github.com/CaiJingLong/flutter_resource_generator) library.
+/// Generate by [asset_generator](https://github.com/fluttercandies/flutter_asset_generator) library.
 /// PLEASE DO NOT EDIT MANUALLY.
 // ignore_for_file: constant_identifier_names
 class R {
   const R._();
 
-  /// ![preview](file://E:\Projects\android-tv\assets\animations\lottie\language.json)
+  /// ![preview](file:///Users/ds/mawaqit/assets/animations/lottie/language.json)
   static const String ASSETS_ANIMATIONS_LOTTIE_LANGUAGE_JSON = 'assets/animations/lottie/language.json';
 
-  /// ![preview](file://E:\Projects\android-tv\assets\animations\lottie\search.json)
+  /// ![preview](file:///Users/ds/mawaqit/assets/animations/lottie/search.json)
   static const String ASSETS_ANIMATIONS_LOTTIE_SEARCH_JSON = 'assets/animations/lottie/search.json';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/animations/lottie/settings.json)
   static const String ASSETS_ANIMATIONS_LOTTIE_SETTINGS_JSON = 'assets/animations/lottie/settings.json';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/animations/lottie/theme.json)
   static const String ASSETS_ANIMATIONS_LOTTIE_THEME_JSON = 'assets/animations/lottie/theme.json';
 
-  /// ![preview](file://E:\Projects\android-tv\assets\animations\lottie\theme.json)
-  static const String ASSETS_ANIMATIONS_LOTTIE_CONFIG_JSON = 'assets/animations/lottie/settings.json';
-
-  /// ![preview](file://E:\Projects\android-tv\assets\animations\lottie\welcome.json)
+  /// ![preview](file:///Users/ds/mawaqit/assets/animations/lottie/welcome.json)
   static const String ASSETS_ANIMATIONS_LOTTIE_WELCOME_JSON = 'assets/animations/lottie/welcome.json';
 
-  /// ![preview](file://E:\Projects\android-tv\assets\animations\rive\mawaqit_logo_animation1.riv)
+  /// ![preview](file:///Users/ds/mawaqit/assets/animations/rive/mawaqit_logo_animation1.riv)
   static const String ASSETS_ANIMATIONS_RIVE_MAWAQIT_LOGO_ANIMATION1_RIV =
       'assets/animations/rive/mawaqit_logo_animation1.riv';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/backgrounds/quran_background.png)
   static const String ASSETS_BACKGROUNDS_BACKGROUND_ADHKAR_JPG = 'assets/backgrounds/background-adhkar.jpg';
 
   static const String ASSETS_BACKGROUNDS_QURAN_BACKGROUND_PNG = 'assets/backgrounds/quran_background.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/backgrounds/splash_screen_5.png)
   static const String ASSETS_BACKGROUNDS_SPLASH_SCREEN_5_PNG = 'assets/backgrounds/splash_screen_5.png';
 
-  /// ![preview](file://E:\Projects\android-tv\assets\cfg\configuration.json)
+  /// ![preview](file:///Users/ds/mawaqit/assets/cfg/configuration.json)
   static const String ASSETS_CFG_CONFIGURATION_JSON = 'assets/cfg/configuration.json';
 
-  /// ![preview](file://E:\Projects\android-tv\assets\cfg\settings.json)
+  /// ![preview](file:///Users/ds/mawaqit/assets/cfg/settings.json)
   static const String ASSETS_CFG_SETTINGS_JSON = 'assets/cfg/settings.json';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/icon/download.svg)
   static const String ASSETS_ICON_DOWNLOAD_SVG = 'assets/icon/download.svg';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/icon/love.svg)
   static const String ASSETS_ICON_LOVE_SVG = 'assets/icon/love.svg';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/icon/pause.svg)
   static const String ASSETS_ICON_PAUSE_SVG = 'assets/icon/pause.svg';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/icon/repeat.svg)
   static const String ASSETS_ICON_REPEAT_SVG = 'assets/icon/repeat.svg';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/icon/shuffle.svg)
   static const String ASSETS_ICON_SHUFFLE_SVG = 'assets/icon/shuffle.svg';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/icon/skip_next.svg)
   static const String ASSETS_ICON_SKIP_NEXT_SVG = 'assets/icon/skip_next.svg';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/icon/skip_previous.svg)
   static const String ASSETS_ICON_SKIP_PREVIOUS_SVG = 'assets/icon/skip_previous.svg';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/ar.png)
   static const String ASSETS_IMG_FLAG_AR_PNG = 'assets/img/flag/ar.png';
 
-  /// ![preview](file://E:\Projects\android-tv\assets\img\flag\ba.png)
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/ba.png)
   static const String ASSETS_IMG_FLAG_BA_PNG = 'assets/img/flag/ba.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/bg.png)
   static const String ASSETS_IMG_FLAG_BG_PNG = 'assets/img/flag/bg.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/bn.png)
   static const String ASSETS_IMG_FLAG_BN_PNG = 'assets/img/flag/bn.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/bs.png)
+  static const String ASSETS_IMG_FLAG_BS_PNG = 'assets/img/flag/bs.png';
+
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/ca.png)
   static const String ASSETS_IMG_FLAG_CA_PNG = 'assets/img/flag/ca.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/cs.png)
   static const String ASSETS_IMG_FLAG_CS_PNG = 'assets/img/flag/cs.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/da.png)
   static const String ASSETS_IMG_FLAG_DA_PNG = 'assets/img/flag/da.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/de.png)
   static const String ASSETS_IMG_FLAG_DE_PNG = 'assets/img/flag/de.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/el.png)
   static const String ASSETS_IMG_FLAG_EL_PNG = 'assets/img/flag/el.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/en.png)
   static const String ASSETS_IMG_FLAG_EN_PNG = 'assets/img/flag/en.png';
 
-  /// ![preview](file://E:\Projects\android-tv\assets\img\flag\es.png)
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/es.png)
   static const String ASSETS_IMG_FLAG_ES_PNG = 'assets/img/flag/es.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/et.png)
   static const String ASSETS_IMG_FLAG_ET_PNG = 'assets/img/flag/et.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/fa.png)
+  static const String ASSETS_IMG_FLAG_FA_PNG = 'assets/img/flag/fa.png';
+
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/fi.png)
   static const String ASSETS_IMG_FLAG_FI_PNG = 'assets/img/flag/fi.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/fr.png)
   static const String ASSETS_IMG_FLAG_FR_PNG = 'assets/img/flag/fr.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/he.png)
   static const String ASSETS_IMG_FLAG_HE_PNG = 'assets/img/flag/he.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/hr.png)
   static const String ASSETS_IMG_FLAG_HR_PNG = 'assets/img/flag/hr.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/hu.png)
   static const String ASSETS_IMG_FLAG_HU_PNG = 'assets/img/flag/hu.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/id.png)
   static const String ASSETS_IMG_FLAG_ID_PNG = 'assets/img/flag/id.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/it.png)
   static const String ASSETS_IMG_FLAG_IT_PNG = 'assets/img/flag/it.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/ja.png)
   static const String ASSETS_IMG_FLAG_JA_PNG = 'assets/img/flag/ja.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/ko.png)
   static const String ASSETS_IMG_FLAG_KO_PNG = 'assets/img/flag/ko.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/ku.png)
+  static const String ASSETS_IMG_FLAG_KU_PNG = 'assets/img/flag/ku.png';
+
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/lt.png)
   static const String ASSETS_IMG_FLAG_LT_PNG = 'assets/img/flag/lt.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/lv.png)
   static const String ASSETS_IMG_FLAG_LV_PNG = 'assets/img/flag/lv.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/ml.png)
   static const String ASSETS_IMG_FLAG_ML_PNG = 'assets/img/flag/ml.png';
 
-  /// ![preview](file://E:\Projects\android-tv\assets\img\flag\nl.png)
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/ms.png)
+  static const String ASSETS_IMG_FLAG_MS_PNG = 'assets/img/flag/ms.png';
+
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/nl.png)
   static const String ASSETS_IMG_FLAG_NL_PNG = 'assets/img/flag/nl.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/no.png)
   static const String ASSETS_IMG_FLAG_NO_PNG = 'assets/img/flag/no.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/pl.png)
   static const String ASSETS_IMG_FLAG_PL_PNG = 'assets/img/flag/pl.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/pt.png)
   static const String ASSETS_IMG_FLAG_PT_PNG = 'assets/img/flag/pt.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/ro.png)
   static const String ASSETS_IMG_FLAG_RO_PNG = 'assets/img/flag/ro.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/ru.png)
   static const String ASSETS_IMG_FLAG_RU_PNG = 'assets/img/flag/ru.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/sl.png)
   static const String ASSETS_IMG_FLAG_SL_PNG = 'assets/img/flag/sl.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/sq.png)
   static const String ASSETS_IMG_FLAG_SQ_PNG = 'assets/img/flag/sq.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/sv.png)
   static const String ASSETS_IMG_FLAG_SV_PNG = 'assets/img/flag/sv.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/ta.png)
   static const String ASSETS_IMG_FLAG_TA_PNG = 'assets/img/flag/ta.png';
 
-  /// ![preview](file://E:\Projects\android-tv\assets\img\flag\tr.png)
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/tr.png)
   static const String ASSETS_IMG_FLAG_TR_PNG = 'assets/img/flag/tr.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/uk.png)
   static const String ASSETS_IMG_FLAG_UK_PNG = 'assets/img/flag/uk.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/ur.png)
   static const String ASSETS_IMG_FLAG_UR_PNG = 'assets/img/flag/ur.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/flag/zh.png)
   static const String ASSETS_IMG_FLAG_ZH_PNG = 'assets/img/flag/zh.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/icon_exit.png)
   static const String ASSETS_IMG_ICON_EXIT_PNG = 'assets/img/icon_exit.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/img/quran/default_avatar.png)
   static const String ASSETS_IMG_QURAN_DEFAULT_AVATAR_PNG = 'assets/img/quran/default_avatar.png';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/scripts/clean.js)
   static const String ASSETS_SCRIPTS_CLEAN_JS = 'assets/scripts/clean.js';
 
-  /// ![preview](file://E:\Projects\android-tv\assets\svg\mawaqit-logo-text-light.svg)
+  /// ![preview](file:///Users/ds/mawaqit/assets/svg/mawaqit-logo-text-light.svg)
   static const String ASSETS_SVG_MAWAQIT_LOGO_TEXT_LIGHT_SVG = 'assets/svg/mawaqit-logo-text-light.svg';
 
-  /// ![preview](file://E:\Projects\android-tv\assets\svg\mawaqit_logo_light.svg)
+  /// ![preview](file:///Users/ds/mawaqit/assets/svg/mawaqit_logo_light.svg)
   static const String ASSETS_SVG_MAWAQIT_LOGO_LIGHT_SVG = 'assets/svg/mawaqit_logo_light.svg';
 
-  /// ![preview](file://E:\Projects\android-tv\assets\svg\no-phone.svg)
+  /// ![preview](file:///Users/ds/mawaqit/assets/svg/no-phone.svg)
   static const String ASSETS_SVG_NO_PHONE_SVG = 'assets/svg/no-phone.svg';
 
-  /// ![preview](file://E:\Projects\android-tv\assets\svg\no-wi-fi.svg)
+  /// ![preview](file:///Users/ds/mawaqit/assets/svg/no-wi-fi.svg)
   static const String ASSETS_SVG_NO_WI_FI_SVG = 'assets/svg/no-wi-fi.svg';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/voices/adhan/adhan-madina-fajr.mp3)
   static const String ASSETS_VOICES_ADHAN_ADHAN_MADINA_FAJR_MP3 = 'assets/voices/adhan/adhan-madina-fajr.mp3';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/voices/adhan/adhan-madina.mp3)
   static const String ASSETS_VOICES_ADHAN_ADHAN_MADINA_MP3 = 'assets/voices/adhan/adhan-madina.mp3';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/voices/adhan/bip.mp3)
   static const String ASSETS_VOICES_ADHAN_BIP_MP3 = 'assets/voices/adhan/bip.mp3';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/voices/duaa_iftar.mp3)
   static const String ASSETS_VOICES_DUAA_IFTAR_MP3 = 'assets/voices/duaa_iftar.mp3';
 
+  /// ![preview](file:///Users/ds/mawaqit/assets/voices/takbir-aid.mp3)
   static const String ASSETS_VOICES_TAKBIR_AID_MP3 = 'assets/voices/takbir-aid.mp3';
 }
