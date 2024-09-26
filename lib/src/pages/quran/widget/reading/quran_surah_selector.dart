da1ac9fecd7fe42b37fa69ea768d5b9c1a2bd3ab
Ghassen Ben Zahra <ghassen.benzahra@gmail.com>
2024-09-26 17:40:19 +0100

    extract filter to seperate widget

---
+++ lib/src/pages/quran/widget/reading/quran_surah_selector.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mawaqit/i18n/l10n.dart';
+import 'package:mawaqit/src/state_management/quran/quran/quran_notifier.dart';
+import 'package:mawaqit/src/state_management/quran/reading/quran_reading_notifer.dart';
+
+void showSurahSelector(BuildContext context, int currentPage, ScrollController gridScrollController) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(
+          S.of(context).surahSelector,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        content: Container(
+          width: double.maxFinite,
+          height: MediaQuery.of(context).size.height * 0.8,
+          child: Consumer(
+            builder: (context, ref, _) {
+              final suwarState = ref.watch(quranNotifierProvider);
+
+              return suwarState.when(
+                loading: () => Center(child: CircularProgressIndicator()),
+                error: (err, stack) => Center(child: Text('Error: $err')),
+                data: (quranState) {
+                  final suwar = quranState.suwar;
+
+                  return StatefulBuilder(
+                    builder: (BuildContext context, StateSetter setState) {
+                      return GridView.builder(
+                        controller: gridScrollController,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 4,
+                          childAspectRatio: 2.5 / 1,
+                          crossAxisSpacing: 8,
+                          mainAxisSpacing: 8,
+                        ),
+                        itemCount: suwar.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final surah = suwar[index];
+                          final page = surah.startPage % 2 == 0 ? surah.startPage - 1 : surah.startPage;
+                          return InkWell(
+                            onTap: () {
+                              ref.read(quranReadingNotifierProvider.notifier).updatePage(page);
+                              Navigator.of(context).pop();
+                            },
+                            child: Container(
+                              height: 40,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(6.0),
+                                border: Border.all(
+                                  color: Theme.of(context).dividerColor,
+                                  width: 1,
+                                ),
+                              ),
+                              child: Text(
+                                "${surah.id}- ${surah.name}",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      );
+    },
+  );
+}
