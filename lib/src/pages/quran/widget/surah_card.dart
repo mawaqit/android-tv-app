95947fdf7cb6cc747b792b31531c7564653a64c9
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2024-10-07 15:10:45 +0300

    feat: download all and single Surah (#1355)
    * rebased quran download missing focus
    
    * add missing methods
    
    * refactor: enhance focus node management for the screen
    
    * remove the hover color
    
    * feat: add localization
    
    * refactor: enhance focus node, download only only works
    
    - **Removed**: The `RiwayatModel` class, which has been deemed unnecessary in the current architecture.
    - **Updated**: Various files across the codebase to replace occurrences of `riwayahId` with `moshafId`, reflecting the new terminology.
    - **Changes in Data Handling**: Adjusted methods in `reciter_local_data_source.dart`, `quran_audio_player_notifier.dart`, and related classes to ensure all functionality correctly references `moshafId` instead of `riwayahId`.
    - **UI Updates**: Updated UI code in `reciter_selection_screen.dart`, `surah_selection_screen.dart`, and `surah_card.dart` to use `moshaf` instead of `riwayah`, ensuring consistency throughout the user interface.
    - **Improved Readability**: Enhanced code clarity by renaming variables and methods where needed, making future maintenance easier.
    
    This refactor aims to streamline the codebase and align with the new vocabulary for Quranic text recitation, improving overall code quality and maintainability.
    
    * refactor: Download Audio Functionality: Introduce SurahDownloadInfo and Refactor State Management
    
    - **Added**: `SurahDownloadInfo` class to encapsulate download progress for individual Surahs, improving clarity and maintainability.
    - **Refactored**: `DownloadAudioQuranState` to include `reciterId`, `moshafId`, and a list of `downloadingSuwar`, simplifying the state management for downloads.
    - **Updated**: Methods in `DownloadStateNotifier` to utilize the new `SurahDownloadInfo` structure for tracking download progress.
    - **Modified**: The `surah_selection_screen` to integrate the refactored state management, ensuring the UI reflects download progress accurately for each Surah.
    - **Improved**: Download logic in `quran_audio_player_notifier` and related files to utilize the new `DownloadStateProviderParameter`, enhancing the modularity of the download process.
    
    * fix: creating multiple provider for the downloading
    
    * fix: reformat
    
    * fix: late initialize of the `playlist` at Quran audio player
    
    * refactor: remove not needed and enhance focusing in the surah playing screen
    
    * fix: selecting the back at the quran player screen
    
    * feat: add single surah download at the surah player
    
    * change download string
    
    ---------
    
    Co-authored-by: Ghassen Ben Zahra <ghassen.benzahra@gmail.com>

--- lib/src/pages/quran/widget/surah_card.dart
+++ lib/src/pages/quran/widget/surah_card.dart
@@ -1,58 +1,177 @@
 import 'package:flutter/material.dart';
-import 'package:mawaqit/src/helpers/RelativeSizes.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mawaqit/i18n/l10n.dart';
 import 'package:sizer/sizer.dart';
 
-class SurahCard extends StatelessWidget {
+class SurahCard extends ConsumerStatefulWidget {
   final String surahName;
   final int surahNumber;
   final int? verses;
-  final bool isSelected;
   final VoidCallback onTap;
+  final bool isDownloaded;
+  final double downloadProgress;
+  final VoidCallback? onDownloadTap;
+  final int index;
 
   const SurahCard({
     required this.surahName,
     required this.surahNumber,
-    required this.isSelected,
     required this.onTap,
+    required this.index,
+    this.isDownloaded = false,
+    required this.downloadProgress,
+    required this.onDownloadTap,
     this.verses,
   });
 
   @override
+  ConsumerState<SurahCard> createState() => _SurahCardState();
+}
+
+class _SurahCardState extends ConsumerState<SurahCard> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 1.05).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          color: isSelected ? Colors.white.withOpacity(0.4) : Colors.white.withOpacity(0.2),
-          borderRadius: BorderRadius.circular(14),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                '$surahNumber. $surahName',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 12.sp,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              SizedBox(height: 8),
-              verses == null
-                  ? Container()
-                  : Text(
-                      '$verses verses',
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.white70,
+    return InkWell(
+      autofocus: widget.index == 0,
+      onTap: widget.onTap,
+      onHover: (isHovering) {
+        if (isHovering) {
+          _animationController.forward();
+        } else {
+          _animationController.reverse();
+        }
+      },
+      onFocusChange: (hasFocus) {
+        if (hasFocus) {
+          _animationController.forward();
+        } else {
+          _animationController.reverse();
+        }
+      },
+      child: AnimatedBuilder(
+        animation: _scaleAnimation,
+        builder: (context, child) {
+          return Transform.scale(
+            scale: _scaleAnimation.value,
+            child: Builder(
+              builder: (context) {
+                return Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color:
+                            Focus.of(context).hasFocus ? Colors.white.withOpacity(0.4) : Colors.white.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(14),
+                      ),
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '${widget.surahNumber}. ${widget.surahName}',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 10.sp,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            if (widget.verses != null) SizedBox(height: 8),
+                            if (widget.verses != null)
+                              Text(
+                                '${widget.verses} verses',
+                                style: TextStyle(
+                                  fontSize: 10.sp,
+                                  color: Colors.white70,
+                                ),
+                              ),
+                          ],
+                        ),
                       ),
                     ),
-            ],
-          ),
-        ),
+                    Positioned(
+                      top: 0,
+                      right: 0,
+                      child: _buildDownloadTag(),
+                    ),
+                  ],
+                );
+              },
+            ),
+          );
+        },
       ),
     );
+  }
+
+  Widget _buildDownloadTag() {
+    if (widget.isDownloaded && widget.downloadProgress == 0) {
+      return InkWell(
+        onTap: widget.onDownloadTap,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 5.sp, vertical: 2.sp),
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.6),
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(14),
+              bottomLeft: Radius.circular(14),
+            ),
+          ),
+          child: Text(
+            S.of(context).downloaded,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 8.sp,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      );
+    } else if (!widget.isDownloaded && widget.downloadProgress > 0 && widget.downloadProgress < 1) {
+      return InkWell(
+        onTap: widget.onDownloadTap,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 5.sp, vertical: 2.sp),
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.6),
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(14),
+              bottomLeft: Radius.circular(14),
+            ),
+          ),
+          child: Text(
+            '${(widget.downloadProgress * 100).toInt()}%',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 8.sp,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      );
+    } else {
+      return Container();
+    }
   }
 }
