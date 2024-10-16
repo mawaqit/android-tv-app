4fe2c08dc9491a674566d3146f468f3b66fb8472
Ghassen Ben Zahra <ghassen.benzahra@gmail.com>
2024-10-16 15:45:04 +0100

    initialize shared pref before fetching mosque

--- lib/src/helpers/SharedPref.dart
+++ lib/src/helpers/SharedPref.dart
@@ -1,24 +1,34 @@
 import 'dart:convert';
-
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPref {
   Future<dynamic> read(String key) async {
     final prefs = await SharedPreferences.getInstance();
-    final value = prefs.getString(key);
 
-    if (value == null) return null;
+    if (prefs.containsKey(key)) {
+      if (prefs.getBool(key) != null) {
+        return prefs.getBool(key);
+      } else {
+        final value = prefs.getString(key);
+        return value != null ? json.decode(value) : null;
+      }
+    }
 
-    return json.decode(value);
+    return null;
   }
 
-  Future<void> save(String key, value) async {
+  Future<void> save(String key, dynamic value) async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.setString(key, json.encode(value));
+
+    if (value is bool) {
+      await prefs.setBool(key, value);
+    } else {
+      await prefs.setString(key, json.encode(value));
+    }
   }
 
   Future<void> remove(String key) async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.remove(key);
+    await prefs.remove(key);
   }
 }
