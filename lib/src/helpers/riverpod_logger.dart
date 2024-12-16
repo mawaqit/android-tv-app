d99d0f19506aa0f927245f4995eff5efeabb3825
Yassin <yassinnouh21@gmail.com>
2024-12-16 20:34:00 +0200

    show logs in riverpod using print

--- lib/src/helpers/riverpod_logger.dart
+++ lib/src/helpers/riverpod_logger.dart
@@ -27,7 +27,7 @@ class RiverpodLogger extends ProviderObserver {
     Object? newValue,
     ProviderContainer container,
   ) {
-    log('''
+    print('''
        {
           "provider": "${provider.name ?? provider.runtimeType}",
           "newValue": "$newValue",
