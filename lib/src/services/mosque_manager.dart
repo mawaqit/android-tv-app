b151723c30eefc745ffc199b41fbf4f20887e332
Ghassen Ben Zahra <ghassen.benzahra@gmail.com>
2025-01-20 15:18:16 +0100

    reenable screen lock feature

--- lib/src/services/mosque_manager.dart
+++ lib/src/services/mosque_manager.dart
@@ -219,7 +219,7 @@ class MosqueManager extends ChangeNotifier with WeatherMixin, AudioMixin, Mosque
         final today = useTomorrowTimes ? AppDateTime.tomorrow() : AppDateTime.now();
         final timeShiftManager = TimeShiftManager();
 
-        if (isDeviceRooted && isToggleScreenActivated && timeShiftManager.isLauncherInstalled) {
+        if (isDeviceRooted && isToggleScreenActivated) {
           ToggleScreenFeature.handleDailyRescheduling(
             isIshaFajrOnly: isIshaFajrOnly,
             timeStrings: e.dayTimesStrings(today, salahOnly: false),
