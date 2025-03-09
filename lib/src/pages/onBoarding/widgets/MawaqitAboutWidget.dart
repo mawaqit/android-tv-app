f0b42c0c1bf8181b379ae406a0084ea6c0909002
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2025-03-09 23:27:08 +0200

    feat: improve layout and font sizes in OnBoardingMawaqitAboutWidget for better responsiveness on different devices

--- lib/src/pages/onBoarding/widgets/MawaqitAboutWidget.dart
+++ lib/src/pages/onBoarding/widgets/MawaqitAboutWidget.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fpdart/fpdart.dart' as fp;
 import 'package:mawaqit/i18n/l10n.dart';
+import 'package:sizer/sizer.dart';
 
 class OnBoardingMawaqitAboutWidget extends StatefulWidget {
   const OnBoardingMawaqitAboutWidget({
@@ -21,56 +22,58 @@ class OnBoardingMawaqitAboutWidget extends StatefulWidget {
 class _OnBoardingMawaqitAboutWidgetState extends State<OnBoardingMawaqitAboutWidget> {
   @override
   void initState() {
+    super.initState();
     widget.nextButtonFocusNode.fold(
       () => null,
       (focusNode) => focusNode.requestFocus(),
     );
-    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     final themeData = Theme.of(context);
+    final isTablet = MediaQuery.of(context).size.width > 600;
 
     return Focus(
       autofocus: true,
       onKey: (FocusNode node, RawKeyEvent event) {
-        print(event.logicalKey);
-
         if (event.isKeyPressed(LogicalKeyboardKey.select) || event.isKeyPressed(LogicalKeyboardKey.enter)) {
           widget.onNext?.call();
           return KeyEventResult.handled;
         }
-
         return KeyEventResult.ignored;
       },
       child: Align(
-        alignment: Alignment(0, -.3),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              S.of(context).mawaqitWelcome,
-              style: TextStyle(
-                fontSize: 25.0,
-                fontWeight: FontWeight.w700,
-                color: themeData.brightness == Brightness.dark ? Colors.white70 : themeData.primaryColor,
-              ),
-            ),
-            SizedBox(height: 10),
-            Flexible(
-              fit: FlexFit.loose,
-              child: AutoSizeText(
-                S.of(context).mawaqitDesc,
-                stepGranularity: 1,
-                textAlign: TextAlign.justify,
+        alignment: const Alignment(0, -.3),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 5.w),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                S.of(context).mawaqitWelcome,
                 style: TextStyle(
-                  fontSize: 19,
-                  color: themeData.brightness == Brightness.dark ? Colors.white60 : themeData.primaryColor,
+                  fontSize: isTablet ? 16.sp : 20.sp,
+                  fontWeight: FontWeight.w700,
+                  color: themeData.brightness == Brightness.dark ? Colors.white70 : themeData.primaryColor,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 2.h),
+              Flexible(
+                fit: FlexFit.tight,
+                child: AutoSizeText(
+                  S.of(context).mawaqitDesc,
+                  stepGranularity: 1,
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    fontSize: isTablet ? 10.sp : 12.sp,
+                    color: themeData.brightness == Brightness.dark ? Colors.white60 : themeData.primaryColor,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
