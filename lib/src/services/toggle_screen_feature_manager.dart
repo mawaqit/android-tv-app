78ad839180c71096ff13c065f31592a430e307e2
Ghassen Ben Zahra <ghassen.benzahra@gmail.com>
2025-04-05 18:12:36 +0100

    code format

--- lib/src/services/toggle_screen_feature_manager.dart
+++ lib/src/services/toggle_screen_feature_manager.dart
@@ -50,17 +50,13 @@ class ToggleScreenFeature {
   ToggleScreenFeature._internal();
   static Future<void> initialize() async {}
 
-  static const String _scheduledTimersKey =
-      TurnOnOffTvConstant.kScheduledTimersKey;
+  static const String _scheduledTimersKey = TurnOnOffTvConstant.kScheduledTimersKey;
   static final Map<String, List<Timer>> _scheduledTimers = {};
   static const String _scheduledInfoKey = 'scheduled_info_key';
   static List<TimerScheduleInfo> _scheduleInfoList = [];
 
   static Future<void> scheduleToggleScreen(
-      bool isfajrIshaonly,
-      List<String> timeStrings,
-      int beforeDelayMinutes,
-      int afterDelayMinutes) async {
+      bool isfajrIshaonly, List<String> timeStrings, int beforeDelayMinutes, int afterDelayMinutes) async {
     try {
       await cancelAllScheduledTimers();
 
@@ -133,9 +129,7 @@ class ToggleScreenFeature {
     final today = AppDateTime.now();
     final isFeatureActive = await getToggleFeatureState();
 
-    final shouldReschedule = lastEventDate != null &&
-        lastEventDate.day != today.day &&
-        isFeatureActive;
+    final shouldReschedule = lastEventDate != null && lastEventDate.day != today.day && isFeatureActive;
     return shouldReschedule;
   }
 
@@ -176,18 +170,15 @@ class ToggleScreenFeature {
       final hour = int.parse(parts[0]);
       final minute = int.parse(parts[1]);
 
-      DateTime scheduledDateTime =
-          DateTime(now.year, now.month, now.day, hour, minute);
+      DateTime scheduledDateTime = DateTime(now.year, now.month, now.day, hour, minute);
       if (scheduledDateTime.isBefore(now)) {
         scheduledDateTime = scheduledDateTime.add(Duration(days: 1));
       }
 
       // Schedule screen on
-      final beforeScheduleTime =
-          scheduledDateTime.subtract(Duration(minutes: beforeDelayMinutes));
+      final beforeScheduleTime = scheduledDateTime.subtract(Duration(minutes: beforeDelayMinutes));
       if (beforeScheduleTime.isAfter(now)) {
-        final uniqueIdOn =
-            'screenOn_${timeString}_${DateTime.now().millisecondsSinceEpoch}';
+        final uniqueIdOn = 'screenOn_${timeString}_${DateTime.now().millisecondsSinceEpoch}';
 
         await WorkManagerService.registerScreenTask(
           uniqueIdOn,
@@ -197,10 +188,8 @@ class ToggleScreenFeature {
       }
 
       // Schedule screen off
-      final afterScheduleTime =
-          scheduledDateTime.add(Duration(minutes: afterDelayMinutes));
-      final uniqueIdOff =
-          'screenOff_${timeString}_${DateTime.now().millisecondsSinceEpoch}';
+      final afterScheduleTime = scheduledDateTime.add(Duration(minutes: afterDelayMinutes));
+      final uniqueIdOff = 'screenOff_${timeString}_${DateTime.now().millisecondsSinceEpoch}';
 
       await WorkManagerService.registerScreenTask(
         uniqueIdOff,
@@ -220,8 +209,7 @@ class ToggleScreenFeature {
 
   static Future<bool> getToggleFeatureState() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final state =
-        prefs.getBool(TurnOnOffTvConstant.kActivateToggleFeature) ?? false;
+    final state = prefs.getBool(TurnOnOffTvConstant.kActivateToggleFeature) ?? false;
     return state;
   }
 
@@ -239,22 +227,19 @@ class ToggleScreenFeature {
 
   static Future<bool> checkEventsScheduled() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final isEventsSet =
-        prefs.getBool(TurnOnOffTvConstant.kIsEventsSet) ?? false;
+    final isEventsSet = prefs.getBool(TurnOnOffTvConstant.kIsEventsSet) ?? false;
     logger.d("value$isEventsSet");
     return isEventsSet;
   }
 
   static Future<void> setLastEventDate(DateTime date) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString(
-        TurnOnOffTvConstant.kLastEventDate, date.toIso8601String());
+    await prefs.setString(TurnOnOffTvConstant.kLastEventDate, date.toIso8601String());
   }
 
   static Future<DateTime?> getLastEventDate() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    final lastEventDateString =
-        prefs.getString(TurnOnOffTvConstant.kLastEventDate);
+    final lastEventDateString = prefs.getString(TurnOnOffTvConstant.kLastEventDate);
     if (lastEventDateString != null) {
       return DateTime.parse(lastEventDateString);
     } else {
