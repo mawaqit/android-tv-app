590143c62ec926f2fc4efe700e4935df7af42c58
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2025-06-14 11:56:35 +0300

    Feat/add brazilian portugal (#1759)
    * feat: add Brazilian Portuguese flag assets
    
    * feat: add transformLocaleToString method for Locale representation
    
    * feat: enhance language support for Brazilian and Portuguese variants
    
    * feat: improve language selection logic by using Locale objects
    
    * fix: remove redundant Brazilian and Portuguese language entries
    
    * fix formatting
    
    * change pt flag
    
    ---------
    
    Co-authored-by: Ghassen Ben Zahra <ghassen.benzahra@gmail.com>
    (cherry picked from commit 16f45b2b9cb6030342cca88ad0c9118bc804b60e)

--- lib/src/pages/onBoarding/widgets/language_selector_widget.dart
+++ lib/src/pages/onBoarding/widgets/language_selector_widget.dart
@@ -4,6 +4,7 @@ import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mawaqit/i18n/AppLanguage.dart';
 import 'package:mawaqit/i18n/l10n.dart';
+import 'package:mawaqit/src/helpers/LocaleHelper.dart';
 import 'package:mawaqit/src/helpers/mawaqit_icons_icons.dart';
 import 'package:provider/provider.dart';
 
@@ -70,11 +71,12 @@ class LanguageSelector extends StatelessWidget {
               separatorBuilder: (BuildContext context, int index) =>
                   Divider(height: 1).animate().fade(delay: .7.seconds),
               itemBuilder: (BuildContext context, int index) {
-                final Locale locale = Locale(languages[index]);
+                final String languageCode = languages[index];
+                final Locale locale = Locale(languageCode);
                 return LanguageTile(
-                  onTap: () => onSelect(locale.languageCode),
+                  onTap: () => onSelect(languageCode),
                   locale: locale,
-                  isSelected: isSelected(locale.languageCode),
+                  isSelected: isSelected(languageCode),
                   isIconActivated: isIconActivated,
                 );
               },
@@ -128,9 +130,9 @@ class _LanguageTileState extends State<LanguageTile> {
           child: ListTile(
             dense: true,
             textColor: widget.isSelected ? Colors.white : null,
-            leading: widget.isIconActivated ? flagIcon(widget.locale.languageCode) : null,
+            leading: widget.isIconActivated ? flagIcon(LocaleHelper.transformLocaleToString(widget.locale)) : null,
             title: Text(
-              appLanguage.combinedLanguageName(widget.locale.languageCode),
+              appLanguage.combinedLanguageName(LocaleHelper.transformLocaleToString(widget.locale)),
               style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
             ),
             trailing: widget.isSelected ? Icon(MawaqitIcons.icon_checked, color: Colors.white) : null,
