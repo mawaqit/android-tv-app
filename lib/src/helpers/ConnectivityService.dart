25db9722471f9925403339617b5ce7c990c19d14
Yassin <yassinnouh21@gmail.com>
2025-06-29 09:28:09 +0300

    fix: add debug logging for connectivity results in ConnectivityService

--- lib/src/helpers/ConnectivityService.dart
+++ lib/src/helpers/ConnectivityService.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:mawaqit/src/domain/model/connectivity_status.dart';
+import 'dart:developer' as developer;
 
 class ConnectivityService {
   final StreamController<ConnectivityStatus> connectionStatusController = StreamController<ConnectivityStatus>();
@@ -9,8 +10,13 @@ class ConnectivityService {
 
   ConnectivityService() {
     _subscription = Connectivity().onConnectivityChanged.listen((List<ConnectivityResult> results) {
+      // Debug logging to see what connectivity results we're getting
+      developer.log('ConnectivityService: Received results: $results');
+
       // Determine the current status based on the list of connectivity results
       final status = _getStatusFromResults(results);
+      developer.log('ConnectivityService: Mapped to status: $status');
+
       connectionStatusController.add(status);
     });
   }
@@ -20,6 +26,8 @@ class ConnectivityService {
       return ConnectivityStatus.Cellular;
     } else if (results.contains(ConnectivityResult.wifi)) {
       return ConnectivityStatus.Wifi;
+    } else if (results.contains(ConnectivityResult.ethernet)) {
+      return ConnectivityStatus.Wifi; // Treat ethernet as connected (same as WiFi for UI purposes)
     } else {
       return ConnectivityStatus.Offline;
     }
