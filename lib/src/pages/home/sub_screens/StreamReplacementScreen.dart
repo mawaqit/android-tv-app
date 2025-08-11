b9236e106a571cec432fdc918a9dbf21cdedfd3a
Ghassen Ben Zahra <ghassen.benzahra@gmail.com>
2025-08-11 16:27:44 +0100

    code format

--- lib/src/pages/home/sub_screens/StreamReplacementScreen.dart
+++ lib/src/pages/home/sub_screens/StreamReplacementScreen.dart
@@ -108,7 +108,6 @@ class StreamReplacementScreen extends ConsumerWidget {
           orElse: () => false,
         );
 
-
     return RawKeyboardListener(
       focusNode: FocusNode(),
       autofocus: true,
